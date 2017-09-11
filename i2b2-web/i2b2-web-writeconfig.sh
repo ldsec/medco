ba8b6faf7b0cfe0c1c79cceab86b02db6d9e4598
Mickaël Misbach <mickael@misba.ch>
2017-09-11 18:29:12 +0200

    add temporary medco-i2b2 deployment for dev by niccolo
    Former-commit-id: 0b061b2a2b719ba5f79bebf84c0d639978548154

--- i2b2-web/i2b2-web-writeconfig.sh
+++ i2b2-web/i2b2-web-writeconfig.sh
@@ -35,6 +35,21 @@ cat > "$LIGHTTPD_WEB_ROOT/i2b2-client/i2b2_config_data.js" <<EOL
 }
 EOL
 
+cat > "$LIGHTTPD_WEB_ROOT/medco-i2b2-client/i2b2_config_data.js" <<EOL
+{
+    urlProxy: "index.php",
+    urlFramework: "js-i2b2/",
+
+    lstDomains: [  {
+        domain: "$I2B2_DOMAIN_NAME",
+        name: "Domain $I2B2_DOMAIN_NAME",
+        urlCellPM: "http://i2b2-server:8080/i2b2/services/PMService/",
+        allowAnalysis: true,
+        debug: false
+    } ]
+}
+EOL
+
 cat > "$LIGHTTPD_WEB_ROOT/index.html" <<EOL
 <html><head><title>I2b2-web</title></head><body>
 
@@ -45,6 +60,7 @@ cat > "$LIGHTTPD_WEB_ROOT/index.html" <<EOL
 <p><a href="/phppgadmin">PhpPgAdmin</a></p>
 <p><a href="http://localhost:9990">WildFly Management</a></p>
 <p><a href="http://localhost:8080/i2b2">I2b2 Axis Management</a></p>
+<p><a href="/medco-i2b2-client">MedCo-I2b2 client (temporary)</a></p>
 </div>
 
 </body>
@@ -54,4 +70,6 @@ EOL
 # i2b2 client and admin whitelist URL
 sed -i "s/\"http:\/\/localhost\"/\"http:\/\/i2b2-server:8080\"/" "$LIGHTTPD_WEB_ROOT/i2b2-admin/index.php"
 sed -i "s/\"http:\/\/localhost\"/\"http:\/\/i2b2-server:8080\"/" "$LIGHTTPD_WEB_ROOT/i2b2-client/index.php"
+# todo: tmp
+sed -i "s/\"http:\/\/localhost\"/\"http:\/\/i2b2-server:8080\"/" "$LIGHTTPD_WEB_ROOT/medco-i2b2-client/index.php"
 
