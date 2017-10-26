c632f53d6c831c7d9753f42b64a8dea37ff0ed45
Mickaël Misbach <mickael@misba.ch>
2017-10-26 16:06:33 +0200

    fix shrine database init script
    Former-commit-id: 503b0420bb49d4c1ceda11b92502ac8b3c2fbfee [formerly ba8c5b3daf8cd89e0bbcfc1f134e27ca119a628d]
    Former-commit-id: c6e6d94f962b2cbba77ddb27c1a7168310210a87

--- shrine-database/initdb-data/00-shrine-database-users.sh
+++ shrine-database/initdb-data/00-shrine-database-users.sh
@@ -4,12 +4,12 @@ set -e
 mysql -p$ADMIN_PASSWORD -u root <<-EOSQL
 
     create database shrine_query_history;
-    grant all privileges on shrine_query_history.* to shrine@% identified by '$DB_PASSWORD';
+    grant all privileges on shrine_query_history.* to 'shrine'@'%' identified by '$DB_PASSWORD';
     create database stewardDB;
-    grant all privileges on stewardDB.* to shrine@% identified by '$DB_PASSWORD';
+    grant all privileges on stewardDB.* to 'shrine'@'%' identified by '$DB_PASSWORD';
     create database adapterAuditDB;
-    grant all privileges on adapterAuditDB.* to shrine@% identified by '$DB_PASSWORD';
+    grant all privileges on adapterAuditDB.* to 'shrine'@'%' identified by '$DB_PASSWORD';
     create database qepAuditDB;
-    grant all privileges on qepAuditDB.* to shrine@% identified by '$DB_PASSWORD';
+    grant all privileges on qepAuditDB.* to 'shrine'@'%' identified by '$DB_PASSWORD';
 
 EOSQL
