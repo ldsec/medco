1d046e16ffa949fc81107ad27fbc5d5f929c55db
Mickaël Misbach <mickael@misba.ch>
2017-10-30 15:57:15 +0100

    shrine: modify keys to have different keystores per nodes
    Former-commit-id: 52b1a9834eb71fed1db82bbf4b3bcbf2f2b323b2 [formerly 5428ea3a35c35e8cf0e2e9019d71c8bb6cf1232c]
    Former-commit-id: c243d0712b8fc400158ef273aef31c37247d169c

--- documentation/generate-configuration-profile.sh
+++ documentation/generate-configuration-profile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+shopt -s nullglob
 
 # usage: bash generate-configuration-profile.sh CONFIGURATION_FOLDER KEYSTORE_PASSWORD NODE_DNS_1 NODE_IP_1 NODE_DNS_2 NODE_IP_3 NODE_DNS_3 NODE_IP_3 ...
 if [ $# -lt 4 ]
@@ -16,29 +17,42 @@ shift
 
 # clean up previous entries
 mkdir -p "$CONF_FOLDER"
-rm -f "$CONF_FOLDER/shrine.keystore" "$CONF_FOLDER/shrine_downstream_nodes.conf" "$CONF_FOLDER/shrine_alias_map.conf"
+rm -f "$CONF_FOLDER"/*shrine.keystore "$CONF_FOLDER"/shrine_downstream_nodes.conf "$CONF_FOLDER"/shrine_alias_map.conf "$CONF_FOLDER"/*.cer
 
+# generate private and keystore for each node
+NODE_IDX="-1"
 while [ $# -gt 0 ]
 do
     NODE_DNS="$1"
     NODE_IP="$2"
+    NODE_IDX=$((NODE_IDX+1))
+    KEYSTORE="$CONF_FOLDER/srv$NODE_IDX-shrine.keystore"
     shift
     shift
 
     # generate the node certificate in the keystore and export it
     keytool -genkeypair -keysize 2048 -alias "$NODE_DNS" -validity 7300 \
         -dname "CN=$NODE_DNS, OU=$NODE_DNS, O=SHRINE Network, L=Lausanne, S=VD, C=CH" -ext "SAN=IP:$NODE_IP" \
-        -keyalg RSA -keypass "$KEYSTORE_PW" -storepass "$KEYSTORE_PW" -keystore "$CONF_FOLDER/shrine.keystore"
+        -keyalg RSA -keypass "$KEYSTORE_PW" -storepass "$KEYSTORE_PW" -keystore "$KEYSTORE"
 
-    keytool -export -alias "$NODE_DNS" -storepass "$KEYSTORE_PW" -file "$CONF_FOLDER/$NODE_DNS.cer" -keystore "$CONF_FOLDER/shrine.keystore"
-    #keytool -import -v -trustcacerts -alias "$NODE_IP" -file "$CONF_FOLDER/$NODE_IP.cer" -keystore "$CONF_FOLDER/shrine.keystore"  -keypass "$KEYSTORE_PW"  -storepass "$KEYSTORE_PW"
-
+    keytool -export -alias "$NODE_DNS" -storepass "$KEYSTORE_PW" -file "$CONF_FOLDER/$NODE_DNS.cer" -keystore "$KEYSTORE"
 
     # add entry in the downstream nodes and alias map
     echo "\"$NODE_DNS\" = \"https://$NODE_DNS:6443/shrine/rest/adapter/requests\"" >> "$CONF_FOLDER/shrine_downstream_nodes.conf"
     echo "\"$NODE_DNS\" = \"$NODE_DNS\"" >> "$CONF_FOLDER/shrine_alias_map.conf"
 
     #todo: unlynx keys
+    keytool -list -v -keystore "$KEYSTORE" -storepass "$KEYSTORE_PW"
+
 done
 
-keytool -list -v -keystore "$CONF_FOLDER/shrine.keystore" -storepass "$KEYSTORE_PW"
+# import certificates of network nodes into the keystores
+for KEYSTORE in "$CONF_FOLDER"/srv*-shrine.keystore
+do
+    for CERTIFICATE in "$CONF_FOLDER"/*.cer
+    do
+        NODE_DNS=$(basename "$CERTIFICATE" ".cer")
+        keytool -noprompt -import -v -trustcacerts -alias "$NODE_DNS" -file "$CERTIFICATE" -keystore "$KEYSTORE"  -keypass "$KEYSTORE_PW"  -storepass "$KEYSTORE_PW" || true
+    done
+done
+
