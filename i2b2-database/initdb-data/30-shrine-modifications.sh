68f52966705c2374dc75bfaeb8513c57226b8130
Mickaël Misbach <mickael@misba.ch>
2017-10-31 10:37:57 +0100

    add shrine test query support
    Former-commit-id: 410f3efd06a5d04593799a3272fdb845ae638f79 [formerly b191a8b1bb297616d75fd2726d8b07697b15d358]
    Former-commit-id: bd2e6ad61927ffda73a9f2a7fa9b00ceb149bd53

--- i2b2-database/initdb-data/30-shrine-modifications.sh
+++ i2b2-database/initdb-data/30-shrine-modifications.sh
@@ -93,7 +93,38 @@ EOSQL
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$I2B2_DOMAIN_NAME" <<-EOSQL
     ALTER TABLE i2b2demodata.patient_dimension ADD COLUMN enc_dummy_flag_cd character(88);
     COMMENT ON COLUMN i2b2demodata.patient_dimension.enc_dummy_flag_cd IS 'base64-encoded encrypted dummy flag (0 or 1)';
-    INSERT INTO i2b2demodata.code_lookup VALUES ('patient_dimension', 'enc_dummy_flag_cd', 'CRC_COLUMN_DESCRIPTOR', 'Encrypted Dummy Flag', NULL, NULL, NULL, NULL, 'NOW()', NULL, 1);
+    INSERT INTO i2b2demodata.code_lookup VALUES
+        ('patient_dimension', 'enc_dummy_flag_cd', 'CRC_COLUMN_DESCRIPTOR', 'Encrypted Dummy Flag', NULL, NULL, NULL,
+        NULL, 'NOW()', NULL, 1);
+EOSQL
+
+
+# add i2b2 test query term for shrine (ontology + database)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$I2B2_DOMAIN_NAME" <<-EOSQL
+    INSERT INTO i2b2metadata.sensitive_tagged VALUES
+        (2, '\\medco\\tagged\\TESTKEY\\', '', 'N', 'LA ', NULL, 'TAG_ID:TESTKEY', NULL, 'concept_cd', 'concept_dimension',
+        'concept_path', 'T', 'LIKE', '\\medco\\tagged\\TESTKEY\\', NULL, NULL, 'NOW()', NULL, NULL, NULL, 'TAG_ID', '@',
+        NULL, NULL, NULL, NULL);
+    INSERT INTO i2b2demodata.concept_dimension VALUES
+        ('\\medco\\tagged\\TESTKEY\\', 'TAG_ID:TESTKEY', NULL, NULL, NULL, NULL, 'NOW()', NULL, NULL);
+    INSERT INTO i2b2demodata.patient_mapping VALUES
+        ('TESTPATIENT', 'TESTSITE', -1, NULL, 'MedCo', NULL, NULL, NULL, 'NOW()', NULL, 1);
+    INSERT INTO i2b2demodata.patient_mapping VALUES
+        ('-1', 'HIVE', -1, 'A', 'HIVE', NULL, 'NOW()', 'NOW()', 'NOW()', 'edu.harvard.i2b2.crc', 1);
+    INSERT INTO i2b2demodata.patient_dimension VALUES
+        (-1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'NOW()', NULL, 1,
+        'FzXxSbBn86gMmF7WT6a4kHDcHrOg3SEkaojcPm7U3qsQp0bhzaLZLYenL/+yNS5j39TFcLU1uSUE5I8tD3Qryw==');
+    INSERT INTO i2b2demodata.encounter_mapping VALUES
+        ('TESTVISIT', 'TESTSITE', 'MedCo', -1, 'TESTPATIENT', 'TESTSITE', NULL, NULL, NULL, NULL, 'NOW()', NULL, 1);
+    INSERT INTO i2b2demodata.encounter_mapping VALUES
+        ('-1', 'HIVE', 'HIVE', -1, 'TESTPATIENT', 'TESTSITE', 'A', NULL, 'NOW()', 'NOW()', 'NOW()', 'edu.harvard.i2b2.crc', 1);
+    INSERT INTO i2b2demodata.visit_dimension VALUES
+        (-1, -1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'NOW()', 'TESTSITE', 1);
+    INSERT INTO i2b2demodata.provider_dimension VALUES
+        ('TESTSITE', '\\medco\\institutions\\TESTSITE\\', 'TESTSITE', NULL, NULL, NULL, 'NOW()', NULL, 1);
+    INSERT INTO i2b2demodata.observation_fact VALUES
+        (-1, -1, 'TAG_ID:TESTKEY', 'TESTSITE', 'NOW()', '@', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'TESTSITE',
+        NULL, NULL, NULL, NULL, 'NOW()', NULL, 1, 1);
 EOSQL
 
 # add demo shrine ontology data TODO: disabled
