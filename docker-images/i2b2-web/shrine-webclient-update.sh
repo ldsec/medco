5415507c88d2534e62dc7632f527c9099390a13f
Mickaël Misbach <mickael@misba.ch>
2017-11-10 17:16:55 +0100

    fix update script
    Former-commit-id: d0479a3f46a2d602898d29a5add24befa1b4070c [formerly 770bc34609f42608a37e6fcb90e6f0b3d7e5ae11]
    Former-commit-id: cdd77b77a72d0093dd8a668825a50730cece7ac8

--- docker-images/i2b2-web/shrine-webclient-update.sh
+++ docker-images/i2b2-web/shrine-webclient-update.sh
@@ -8,7 +8,7 @@ cd "$SHRINE_SRC_DIR"
 git pull
 rm -R "$LIGHTTPD_WEB_ROOT/shrine-client"
 cp -R "$SHRINE_SRC_DIR/shrine-webclient/src/main/html" "$LIGHTTPD_WEB_ROOT/shrine-client"
-bash ./i2b2-web-writeconfig.sh
+bash /opt/i2b2-web-writeconfig.sh
 chown -R www-data:www-data "$LIGHTTPD_WEB_ROOT/shrine-client"
 chmod -R +r "$LIGHTTPD_WEB_ROOT/shrine-client"
 chmod -R +x "$LIGHTTPD_WEB_ROOT/shrine-client"
