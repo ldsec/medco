b315372dc95ab152cb574edc4a738b64aa22721a
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-08-21 16:02:08 +0200

    Still trying to debug
    Former-commit-id: 6539aa708285d68a79ee1ae2ce8e489e7cfc1e63 [formerly 5f01d3f93f56bbc4ca7615285defe70586608c28]
    Former-commit-id: 5ac3109d3889a46015b243da7a9b50e525d9d8b8

--- docker-images/prod/i2b2-web/copy-i2b2-web-binary-prod.sh
+++ docker-images/prod/i2b2-web/copy-i2b2-web-binary-prod.sh
@@ -4,11 +4,11 @@ set -e
 # copy pm cell deployment data to shared volume
 #cp -a -R $BUILD_DIR/i2b2-web/html/* $LIGHTTPD_WEB_ROOT
 
-chgrp -R www-data /opt /etc/lighttpd
-chmod -R g+rwx /opt /etc/lighttpd
-chown -R www-data:www-data "$LIGHTTPD_WEB_ROOT"
-chmod -R +rx "$LIGHTTPD_WEB_ROOT"
-install -d -o www-data -g www-data -m 0750 "/var/run/lighttpd"
-su "www-data" -p -s /bin/bash -c "/opt/i2b2-web-writeconfig.sh"
+#chgrp -R www-data /opt /etc/lighttpd
+#chmod -R g+rwx /opt /etc/lighttpd
+#chown -R www-data:www-data "$LIGHTTPD_WEB_ROOT"
+#chmod -R +rx "$LIGHTTPD_WEB_ROOT"
+#install -d -o www-data -g www-data -m 0750 "/var/run/lighttpd"
+#su "www-data" -p -s /bin/bash -c "/opt/i2b2-web-writeconfig.sh"
 
 lighttpd -D -f /etc/lighttpd/lighttpd.conf
