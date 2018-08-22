3ecb4553297e247c32c09fd0b8ff5846f6b81a14
root <root@iccluster069.iccluster.epfl.ch>
2018-08-22 17:28:36 +0200

    updated copy-shrine-server-binary-dev.sh
    Former-commit-id: 72a9d6cd8ea1fcab8b74dd904499c87bd908f22d [formerly bfcf1d0c3b49f6603fe7c0f619394bb58535918f]
    Former-commit-id: 278c0865a684fcb8a21d0f8d4eab741f2fde3304

--- docker-images/dev/shrine-server/copy-shrine-server-binary-dev.sh
+++ docker-images/dev/shrine-server/copy-shrine-server-binary-dev.sh
@@ -5,5 +5,4 @@ rm -rf $BUILD_DIR/shrine-server/
 mkdir $BUILD_DIR/shrine-server/
 # copy pm cell deployment data to shared volume
 cp -a -R $CATALINA_HOME $BUILD_DIR/shrine-server/
-ls /root/
-cp -a -R /root/.m2 $BUILD_DIR/shrine-server/
+cp -a -R $SHRINE_SRC_DIR $BUILD_DIR/shrine-server/src/
