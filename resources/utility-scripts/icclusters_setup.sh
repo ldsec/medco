8f3bbf69b1a4fb8004ff6b97f558f9460575df14
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-08-22 10:52:47 +0200

    Reverting back web dockerfiles
    Former-commit-id: dc1406eda313ddbf37853b5d606594c5ec7bb0da [formerly 55f40af9c158b887cb7d51a7afffb60d9aeb9f36]
    Former-commit-id: 68a855db8e77f0a7726b297e38c3a79c4a8d7dfb

--- resources/utility-scripts/icclusters_setup.sh
+++ resources/utility-scripts/icclusters_setup.sh
@@ -5,8 +5,7 @@ SERVERS="$@"
 SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for s in $SERVERS; do
-    echo "Setting up iccluster$s.iccluster.epfl.ch..."
-    login=root@iccluster$s.iccluster.epfl.ch
-    #cat install_script.sh | ssh $login /bin/bash
+    echo "Setting up iccluster0$s.iccluster.epfl.ch..."
+    login=root@iccluster0$s.iccluster.epfl.ch
     cat "$SCRIPT_FOLDER"/ubuntu_prereqs_setup.sh | sshpass -p "1" ssh -o StrictHostKeyChecking=no $login /bin/bash
 done
