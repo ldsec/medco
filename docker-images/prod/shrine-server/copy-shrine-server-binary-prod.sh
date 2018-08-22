e595a9d72b9174593c1074e0abb0373ad7c1ad22
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-08-22 17:49:27 +0200

    Debugging Shrine
    Former-commit-id: 068ae714e97deb4b76a576a6052bdd34c8219deb [formerly 40c23d54ca96b6c8aee73fa674318aaa3f51b442]
    Former-commit-id: daaa16c326a99ca6b6647fb83fef1507780d5556

--- docker-images/prod/shrine-server/copy-shrine-server-binary-prod.sh
+++ docker-images/prod/shrine-server/copy-shrine-server-binary-prod.sh
@@ -3,7 +3,7 @@ set -e
 
 # copy pm cell deployment to the right place in the container
 cp -a -rf $BUILD_DIR/shrine-server/* $CATALINA_HOME
-cp -a -rf $BUILD_DIR/shrine-server/src/* $SHRINE_SRC_DIR
+cp -a -rf $BUILD_DIR/shrine-server/src/* $SHRINE_SRC_DIR/
 
 sed -i "s/SHRINE_KEYSTORE_PRIVATE_KEY_ALIAS/srv$NODE_IDX-private/g" "$CATALINA_HOME/conf/server.xml"
 sed -i "s#SHRINE_KEYSTORE_FILE_PATH#$CONF_DIR/srv$NODE_IDX.keystore#g" "$CATALINA_HOME/conf/server.xml"
