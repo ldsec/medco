0f0552a24b705f6039f62b7219cfa16e36ac9024
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-08-22 17:10:08 +0200

    Debugging Shrine
    Former-commit-id: 82f032e50313878785764f08c82a47b29c619a97 [formerly 72151c1eb8068f2493bb89475aae22e32130f0d4]
    Former-commit-id: 33f7a229b81c54d60ceba3980e430f7071a33240

--- docker-images/dev/shrine-server/copy-shrine-server-binary-dev.sh
+++ docker-images/dev/shrine-server/copy-shrine-server-binary-dev.sh
@@ -5,3 +5,5 @@ rm -rf $BUILD_DIR/shrine-server/
 mkdir $BUILD_DIR/shrine-server/
 # copy pm cell deployment data to shared volume
 cp -a -R $CATALINA_HOME $BUILD_DIR/shrine-server/
+ls /root/
+cp -a -R /root/.m2 $BUILD_DIR/shrine-server/
