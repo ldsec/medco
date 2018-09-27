cec50cbcd639a0d87f06e83f98d0642f8de7d3bf
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-09-27 16:05:56 +0200

    Trying to get something to work
    Former-commit-id: 65fc85392318edc85a2433f61726d1e543a82b60 [formerly 44899dc50a704d03816df39e3b625af099171552]
    Former-commit-id: a0b3eaa17734b7078b98c33f6dc10288ecd34c21

--- docker-images/dev/shrine-server/copy-shrine-server-binary-dev.sh
+++ docker-images/dev/shrine-server/copy-shrine-server-binary-dev.sh
@@ -4,6 +4,5 @@ set -e
 rm -rf $BUILD_DIR/shrine-server/
 mkdir $BUILD_DIR/shrine-server/
 # copy pm cell deployment data to shared volume
+
 cp -a -R $CATALINA_HOME $BUILD_DIR/shrine-server/
-cp -a -R $SHRINE_SRC_DIR $BUILD_DIR/shrine-server/src/
-echo $(ls /opt/)
