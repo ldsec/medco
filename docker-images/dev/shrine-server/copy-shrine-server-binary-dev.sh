0425571f150d7c9e9187b15b264e2f76c35fcb88
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-09-28 10:08:21 +0200

    Deleted the split of shrine docker
    Former-commit-id: ba868b9f393071e53b6e920cd43ffadf7269f23b [formerly d3c5e323966f898997648a1ea919d1f4556f6165]
    Former-commit-id: 9c0bd08060d757f4d6ad902fc1934a4ec60ca788

--- docker-images/dev/shrine-server/copy-shrine-server-binary-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-rm -rf $BUILD_DIR/shrine-server/
-mkdir $BUILD_DIR/shrine-server/
-# copy pm cell deployment data to shared volume
-
-cp -a -R $CATALINA_HOME $BUILD_DIR/shrine-server/
