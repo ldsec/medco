a77992184d7471735ebf578f0d653f80e4a8d4fe
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-09-28 10:25:32 +0200

    Deleted previous configuration and compose files
    Former-commit-id: e15124fa9314f78ce95449d5e5308ba1c8a6523b [formerly ef1e53a146e71ddec3d254b923b6526728bd883a]
    Former-commit-id: 4f0ef9179cf2bb3db4f775412b96cf5957de4f31

--- docker-images/unlynx/copy-unlynx-binary.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# copy unlynx binary in the configuration folder (environment variables are available)
-rm -f $UNLYNX_BIN_EXPORT_PATH
-cp -a $(which app) $UNLYNX_BIN_EXPORT_PATH
-chmod 777 $UNLYNX_BIN_EXPORT_PATH
