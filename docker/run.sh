5814495893fba3457e1ae56fbe9c671a2f014dbd
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-10-01 18:06:12 +0200

    Added docker app build
    Former-commit-id: 035955a9b8d585d6bdd7dbba0923f911277156b8

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-docker build .
-#docker run --volume=build:/opt/build-dir
+SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker build -t lca1/medco-loader:medco-loader .
+docker run -v "$SCRIPT_FOLDER":/opt/build-dir --entrypoint sh lca1/medco-loader:medco-loader /copy-medco-loader-binary.sh
