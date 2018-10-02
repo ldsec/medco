d52deeea9f298b2d620665296c70baa128df2bb6
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-10-02 16:47:46 +0200

    Generated test script for loader
    Former-commit-id: 73bf007b6c56986da9de46d6742fde00c13f49c5

--- app/test.sh
+++ app/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bash compileMac.sh && ./medcoLoader -debug 2 v1 -g ../data/i2b2/group.toml --entry 0 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml
+./medco-loader -debug 2 v1 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entry 0 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml
