eae82061474cc26b25b10fe795d7963b2f3795d8
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-10-03 08:47:43 +0200

    Added truncate command to test import script
    Former-commit-id: 9536d21c7165f47b58df007d6492c7fe24194ae1

--- data/i2b2/converted/importData.sh
+++ data/i2b2/converted/importData.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 PGPASSWORD=prigen2017 psql -v ON_ERROR_STOP=1 -h "localhost" -U "postgres" -p 5434 -d "i2b2medco" <<-EOSQL
 BEGIN;
+TRUNCATE TABLE shrine_ont.shrine;
+TRUNCATE TABLE i2b2metadata.i2b2;
+TRUNCATE TABLE i2b2metadata.sensitive_tagged;
+TRUNCATE TABLE i2b2demodata.concept_dimension;
+TRUNCATE TABLE i2b2demodata.modifier_dimension;
+TRUNCATE TABLE i2b2demodata.patient_dimension;
+TRUNCATE TABLE i2b2demodata.visit_dimension;
+TRUNCATE TABLE i2b2demodata.observation_fact;
+
 \copy shrine_ont.shrine FROM 'shrine.csv' ESCAPE '"' DELIMITER ',' CSV HEADER;
 \copy i2b2metadata.i2b2 FROM 'i2b2.csv' ESCAPE '"' DELIMITER ',' CSV HEADER;
 \copy i2b2metadata.sensitive_tagged FROM 'sensitive_tagged.csv' ESCAPE '"' DELIMITER ',' CSV HEADER;
