6a83b99294c1479c401697dba76347636a10b314
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-10-03 08:32:50 +0200

    Added a test script to load data
    Former-commit-id: 3e1a4bb6c28e6d1ea6dfc06bb877bce5f2b9dc15

---
+++ data/i2b2/converted/importData.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+PGPASSWORD=prigen2017 psql -v ON_ERROR_STOP=1 -h "localhost" -U "postgres" -p 5434 -d "i2b2medco" <<-EOSQL
+BEGIN;
+\copy shrine_ont.shrine FROM 'shrine.csv' ESCAPE '"' DELIMITER ',' CSV HEADER;
+\copy i2b2metadata.i2b2 FROM 'i2b2.csv' ESCAPE '"' DELIMITER ',' CSV HEADER;
+\copy i2b2metadata.sensitive_tagged FROM 'sensitive_tagged.csv' ESCAPE '"' DELIMITER ',' CSV HEADER;
+\copy i2b2demodata.concept_dimension FROM 'concept_dimension.csv' ESCAPE '"' DELIMITER ',' CSV HEADER;
+\copy i2b2demodata.modifier_dimension FROM 'modifier_dimension.csv' ESCAPE '"' DELIMITER ',' CSV HEADER;
+\copy i2b2demodata.patient_dimension FROM 'patient_dimension.csv' ESCAPE '"' DELIMITER ',' CSV HEADER;
+\copy i2b2demodata.visit_dimension FROM 'visit_dimension.csv' ESCAPE '"' DELIMITER ',' CSV HEADER;
+\copy i2b2demodata.observation_fact FROM 'observation_fact.csv' ESCAPE '"' DELIMITER ',' CSV HEADER;
+COMMIT;
+EOSQL
