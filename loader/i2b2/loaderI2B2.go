93c5f94df72b4bf09ff47755a44813805cb57d38
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-10-12 14:59:45 +0200

    More quick fixes to the loader .sh
    Former-commit-id: 9bc3b46f63fa71742b240f873aae01dd7977f6a3

--- loader/i2b2/loaderI2B2.go
+++ loader/i2b2/loaderI2B2.go
@@ -275,8 +275,7 @@ func GenerateLoadingDataScript(databaseS loader.DBSettings) error {
 			loading += "TRUNCATE TABLE "+ fI.TableName +";\n"
 			loading += `\copy ` + fI.TableName + ` FROM '` + fI.Path + `' ESCAPE '"' DELIMITER ',' CSV HEADER;`+"\n"
 		} else if strings.HasPrefix(file, "SHRINE_"){
-			rawName := strings.Split(file, "SHRINE_")[1]
-			loading += "CREATE TABLE " + fI.TableName + " AS SELECT * FROM " + OutputFilePaths["LOCAL_"+rawName].TableName + " WHERE 1=2;"+"\n"
+			loading += "CREATE TABLE " + fI.TableName + " AS SELECT * FROM shrine_ont.shrine WHERE 1=2;"+"\n"
 			loading += `\copy ` + fI.TableName + ` FROM '` + fI.Path + `' ESCAPE '"' DELIMITER ',' CSV HEADER;`+"\n"
 		}
 	}
@@ -289,10 +288,10 @@ func GenerateLoadingDataScript(databaseS loader.DBSettings) error {
 	loading += `\copy ` + OutputFilePaths["TABLE_ACCESS_S"].TableName + ` FROM '` + OutputFilePaths["TABLE_ACCESS_S"].Path + `' ESCAPE '"' DELIMITER ',' CSV HEADER;`+"\n"
 
 	loading += "\n"
-	loading += `\copy ` + OutputFilePaths["CONCEPT_DIMENSION"].TableName + `FROM '` + OutputFilePaths["CONCEPT_DIMENSION"].Path + `' ESCAPE '"' DELIMITER ',' CSV HEADER;` + "\n" +
-				`\copy ` + OutputFilePaths["PATIENT_DIMENSION"].TableName + `FROM '` + OutputFilePaths["PATIENT_DIMENSION"].Path + `' ESCAPE '"' DELIMITER ',' CSV HEADER;` + "\n" +
-				`\copy ` + OutputFilePaths["VISIT_DIMENSION"].TableName + `FROM '` + OutputFilePaths["VISIT_DIMENSION"].Path + `' ESCAPE '"' DELIMITER ',' CSV HEADER;` + "\n" +
-				`\copy ` + OutputFilePaths["OBSERVATION_FACT"].TableName + `FROM '` + OutputFilePaths["OBSERVATION_FACT"].Path + `' ESCAPE '"' DELIMITER ',' CSV HEADER;` +"\n"
+	loading += `\copy ` + OutputFilePaths["CONCEPT_DIMENSION"].TableName + ` FROM '` + OutputFilePaths["CONCEPT_DIMENSION"].Path + `' ESCAPE '"' DELIMITER ',' CSV HEADER;` + "\n" +
+				`\copy ` + OutputFilePaths["PATIENT_DIMENSION"].TableName + ` FROM '` + OutputFilePaths["PATIENT_DIMENSION"].Path + `' ESCAPE '"' DELIMITER ',' CSV HEADER;` + "\n" +
+				`\copy ` + OutputFilePaths["VISIT_DIMENSION"].TableName + ` FROM '` + OutputFilePaths["VISIT_DIMENSION"].Path + `' ESCAPE '"' DELIMITER ',' CSV HEADER;` + "\n" +
+				`\copy ` + OutputFilePaths["OBSERVATION_FACT"].TableName + ` FROM '` + OutputFilePaths["OBSERVATION_FACT"].Path + `' ESCAPE '"' DELIMITER ',' CSV HEADER;` +"\n"
 
 	loading += "COMMIT;\n"
 	loading += "EOSQL"
