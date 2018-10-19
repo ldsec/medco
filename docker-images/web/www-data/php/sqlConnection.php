0c683e7ee259f451841874145bbaff737e317770
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-10-19 10:41:08 +0200

    Fixed the test php script
    Former-commit-id: 1c939d88e58ecc6408f7010ceaf765058cc728a8 [formerly b5a7b8af2ecc8e8a0d6917e23f8802b666c98e21]
    Former-commit-id: 287dfcdbed47d9eb7e80d86d00be19addd73f52e

--- docker-images/web/www-data/php/sqlConnection.php
+++ docker-images/web/www-data/php/sqlConnection.php
@@ -1,12 +1,13 @@
 <?php
-header('Access-Control-Allow-Origin: ' getenv('CORS_ALLOW_ORIGIN')); 
+header('Access-Control-Allow-Origin: '.getenv('CORS_ALLOW_ORIGIN')); 
 header('Access-Control-Allow-Credentials: true'); 
 header('Access-Control-Allow-Headers: origin, content-type, accept, authorization'); 
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD'); 
 
-$conn = pg_connect("host=I2B2_DB_HOST port=I2B2_DB_PORT dbname=I2B2_DB_NAME user=I2B2_DB_USER password=I2B2_DB_PW");
+$conn = pg_connect("host=".getenv('I2B2_DB_HOST')." port=".getenv('I2B2_DB_PORT')." dbname=".getenv('I2B2_DB_NAME')." user=".getenv('I2B2_DB_USER')." password=".getenv('I2B2_DB_PW'));
 
 if (!$conn) {
     echo "Error while connecting to the postgres database.";
     exit;
 }
+?> 
--- docker-images/web/www-data/php/test.php
+++ docker-images/web/www-data/php/test.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: ' getenv('CORS_ALLOW_ORIGIN')); 
+header('Access-Control-Allow-Origin: '.getenv('CORS_ALLOW_ORIGIN')); 
 header('Access-Control-Allow-Credentials: true'); 
 header('Access-Control-Allow-Headers: origin, content-type, accept, authorization'); 
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD'); 
@@ -9,10 +9,11 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD');
 include 'sqlConnection.php';
 
 // get the row which contains all the values of the passed annotation
-$query =
-    "SELECT concept_path 
-	 FROM i2b2demodata.concept_dimension
-	 WHERE concept_code = 'TAG_ID:0';"
+$query = "
+	SELECT concept_path 
+    FROM i2b2demodata.concept_dimension
+    WHERE concept_code = 'TAG_ID:0';
+";
 
 $result = pg_query($conn, $query);
 if (!$result) {
@@ -26,4 +27,4 @@ while ($row = pg_fetch_row($result)) {
     $concept_path .= "\"$row[0]\",";
 }
 // drop the last comma and concatenate in json format
-echo "[" . substr($concept_path, 0, -1) . "]";
+echo "[" . substr($concept_path, 0, -1) . "]";?> 
