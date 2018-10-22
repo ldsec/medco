e608340f424a70a80649273297a89f7e55f464a0
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-10-22 13:55:04 +0200

    Added a smaller tcga_bio dataset to test
    Former-commit-id: ae55cdceff432b773d21bee16664381aeb4510f5

--- app/testGenomic.sh
+++ app/testGenomic.sh
@@ -4,18 +4,18 @@
 --ont_genomic ../data/genomic/tcga_cbio/8_mutation_data.csv  \
 --clinical ../data/genomic/tcga_cbio/8_clinical_data.csv \
 --genomic ../data/genomic/tcga_cbio/8_mutation_data.csv \
---dbHost postgresql --dbPort 5432 --dbName i2b2medcosrv0 --dbUser i2b2 --dbPassword i2b2
+--dbHost localhost --dbPort 5432 --dbName i2b2medcosrv0 --dbUser i2b2 --dbPassword i2b2
 
 ./medcoLoader -debug 2 v0 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entryPointIdx 1 \
 --ont_clinical ../data/genomic/tcga_cbio/8_clinical_data.csv --sen ../data/genomic/sensitive.txt \
 --ont_genomic ../data/genomic/tcga_cbio/8_mutation_data.csv  \
 --clinical ../data/genomic/tcga_cbio/8_clinical_data.csv \
 --genomic ../data/genomic/tcga_cbio/8_mutation_data.csv \
---dbHost postgresql --dbPort 5432 --dbName i2b2medcosrv2 --dbUser i2b2 --dbPassword i2b2
+--dbHost localhost --dbPort 5432 --dbName i2b2medcosrv2 --dbUser i2b2 --dbPassword i2b2
 
 ./medcoLoader -debug 2 v0 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entryPointIdx 2 \
 --ont_clinical ../data/genomic/tcga_cbio/8_clinical_data.csv --sen ../data/genomic/sensitive.txt \
 --ont_genomic ../data/genomic/tcga_cbio/8_mutation_data.csv  \
 --clinical ../data/genomic/tcga_cbio/8_clinical_data.csv \
 --genomic ../data/genomic/tcga_cbio/8_mutation_data.csv \
---dbHost postgresql --dbPort 5432 --dbName i2b2medcosrv2 --dbUser i2b2 --dbPassword i2b2
+--dbHost localhost --dbPort 5432 --dbName i2b2medcosrv2 --dbUser i2b2 --dbPassword i2b2
