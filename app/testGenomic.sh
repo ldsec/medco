1ee38329761b91f710f94019639d08fb3f45d020
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-10-22 10:11:09 +0200

    En train de debbuging the genomic loader
    Former-commit-id: da251e78feadd13b148dd0a42216eaa04a0e2554

--- app/testGenomic.sh
+++ app/testGenomic.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
-./medcoLoader -debug 2 v2 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entryPointIdx 0 \
---ont_clinical ./clinical_data.csv --sensitive CANCER_TYPE_DETAILED \
---ont_genomic ./mutation_data.csv  \
---clinical ./80_node0_clinical_data.csv --genomic ./80_node0_mutation_data.csv \
+./medcoLoader -debug 2 v0 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entryPointIdx 0 \
+--ont_clinical ../data/genomic/tcga_cbio/clinical_data.csv --sensitive CANCER_TYPE_DETAILED \
+--ont_genomic ../data/genomic/tcga_cbio/mutation_data.csv  \
+--clinical ../data/genomic/tcga_cbio/manipulations/80_node0_clinical_data.csv \
+--genomic ../data/genomic/tcga_cbio/manipulations/80_node0_mutation_data.csv \
 --dbHost postgresql --dbPort 5432 --dbName i2b2medcosrv0 --dbUser i2b2 --dbPassword i2b2
 
-./medcoLoader -debug 2 v2 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entryPointIdx 1 \
---ont_clinical ./clinical_data.csv --sensitive CANCER_TYPE_DETAILED \
---ont_genomic ./mutation_data.csv  \
---clinical ./80_node1_clinical_data.csv --genomic ./80_node1_mutation_data.csv \
+./medcoLoader -debug 2 v0 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entryPointIdx 1 \
+--ont_clinical ../data/genomic/tcga_cbio/clinical_data.csv --sensitive CANCER_TYPE_DETAILED \
+--ont_genomic ../data/genomic/tcga_cbio/mutation_data.csv  \
+--clinical ../data/genomic/tcga_cbio/manipulations/80_node1_clinical_data.csv \
+--genomic ../data/genomic/tcga_cbio/manipulations/80_node1_mutation_data.csv \
 --dbHost postgresql --dbPort 5432 --dbName i2b2medcosrv2 --dbUser i2b2 --dbPassword i2b2
 
-./medcoLoader -debug 2 v2 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entryPointIdx 2 \
+./medcoLoader -debug 2 v0 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entryPointIdx 2 \
 --ont_clinical ./clinical_data.csv --sensitive CANCER_TYPE_DETAILED \
 --ont_genomic ./mutation_data.csv  \
---clinical ./80_node2_clinical_data.csv --genomic ./80_node2_mutation_data.csv \
+--clinical ../data/genomic/tcga_cbio/manipulations/80_node2_clinical_data.csv \
+--genomic ../data/genomic/tcga_cbio/manipulations/80_node2_mutation_data.csv \
 --dbHost postgresql --dbPort 5432 --dbName i2b2medcosrv2 --dbUser i2b2 --dbPassword i2b2
--- app/testI2B2.sh
+++ app/testI2B2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-./medcoLoader -debug 2 v0 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entry 0 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml \
+./medcoLoader -debug 2 v1 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entry 0 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml \
 --dbHost postgresql  --dbPort 5432 --dbName i2b2medcosrv0 --dbUser i2b2 --dbPassword i2b2
 
-./medcoLoader -debug 2 v0 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entry 1 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml \
+./medcoLoader -debug 2 v1 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entry 1 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml \
 --dbHost postgresql  --dbPort 5432 --dbName i2b2medcosrv1 --dbUser i2b2 --dbPassword i2b2
 
-./medcoLoader -debug 2 v0 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entry 2 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml \
+./medcoLoader -debug 2 v1 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entry 2 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml \
 --dbHost postgresql  --dbPort 5432 --dbName i2b2medcosrv2 --dbUser i2b2 --dbPassword i2b2
