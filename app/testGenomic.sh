66ff78cc3a96f1ee3143996c140651c85b45d349
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-10-22 10:54:34 +0200

    En train de debbuging the genomic loader
    Former-commit-id: 120f300fff958c25bdb61f7bd0bdabd0fb368ffa

--- app/testGenomic.sh
+++ app/testGenomic.sh
@@ -2,20 +2,20 @@
 ./medcoLoader -debug 2 v0 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entryPointIdx 0 \
 --ont_clinical ../data/genomic/tcga_cbio/clinical_data.csv --sensitive CANCER_TYPE_DETAILED \
 --ont_genomic ../data/genomic/tcga_cbio/mutation_data.csv  \
---clinical ../data/genomic/tcga_cbio/manipulations/clinical_data.csv \
---genomic ../data/genomic/tcga_cbio/manipulations/mutation_data.csv \
+--clinical ../data/genomic/tcga_cbio/clinical_data.csv \
+--genomic ../data/genomic/tcga_cbio/mutation_data.csv \
 --dbHost postgresql --dbPort 5432 --dbName i2b2medcosrv0 --dbUser i2b2 --dbPassword i2b2
 
 ./medcoLoader -debug 2 v0 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entryPointIdx 1 \
 --ont_clinical ../data/genomic/tcga_cbio/clinical_data.csv --sensitive CANCER_TYPE_DETAILED \
 --ont_genomic ../data/genomic/tcga_cbio/mutation_data.csv  \
---clinical ../data/genomic/tcga_cbio/manipulations/clinical_data.csv \
---genomic ../data/genomic/tcga_cbio/manipulations/mutation_data.csv \
+--clinical ../data/genomic/tcga_cbio/clinical_data.csv \
+--genomic ../data/genomic/tcga_cbio/mutation_data.csv \
 --dbHost postgresql --dbPort 5432 --dbName i2b2medcosrv2 --dbUser i2b2 --dbPassword i2b2
 
 ./medcoLoader -debug 2 v0 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entryPointIdx 2 \
 --ont_clinical ../data/genomic/tcga_cbio/clinical_data.csv --sensitive CANCER_TYPE_DETAILED \
 --ont_genomic ../data/genomic/tcga_cbio/mutation_data.csv  \
---clinical ../data/genomic/tcga_cbio/manipulations/clinical_data.csv \
---genomic ../data/genomic/tcga_cbio/manipulations/mutation_data.csv \
+--clinical ../data/genomic/tcga_cbio/clinical_data.csv \
+--genomic ../data/genomic/tcga_cbio/mutation_data.csv \
 --dbHost postgresql --dbPort 5432 --dbName i2b2medcosrv2 --dbUser i2b2 --dbPassword i2b2
