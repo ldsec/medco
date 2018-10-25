c4d8557bdaa55a8b2b0e37a2ecf78ff2e3671d94
Mickaël Misbach <mickael@misba.ch>
2018-10-25 16:34:42 +0200

    dinamically generated password for medco PM configuration
    Former-commit-id: 1d19b9f5d4a960e239a8b3294683ad7abe967680 [formerly 7ed800297f51f55a7b39d4b7092d66da3d6c1f94]
    Former-commit-id: 4cf0e5e2e59c32d2a25ea9eadfb1f2248209a01b

--- docker-images/i2b2-medco/sql/55-medco-pm-configuration.sh
+++ docker-images/i2b2-medco/sql/55-medco-pm-configuration.sh
@@ -2,6 +2,10 @@
 set -Eeuo pipefail
 # set up PM for medco
 
+# generate password hashes
+I2B2_SERVICE_PASSWORD_HASH=$(java -classpath "$JBOSS_HOME/I2b2PasswordHash/" I2b2PasswordHash "$I2B2_SERVICE_PASSWORD")
+DEFAULT_USER_PASSWORD_HASH=$(java -classpath "$JBOSS_HOME/I2b2PasswordHash/" I2b2PasswordHash "$DEFAULT_USER_PASSWORD")
+
 psql $PSQL_PARAMS -d "$I2B2_DB_NAME" <<-EOSQL
     -- database lookups
     insert into i2b2hive.ont_db_lookup (c_domain_id, c_project_path, c_owner_id, c_db_fullschema, c_db_datasource, c_db_servertype, c_db_nicename)
@@ -16,11 +20,11 @@ psql $PSQL_PARAMS -d "$I2B2_DB_NAME" <<-EOSQL
     --    values ('CRC', '/MedCo', 'MedCo Federated Query', 'REST', 'https://tbd:6443/shrine/rest/i2b2/', 1, 'A');
 
     INSERT INTO i2b2pm.PM_USER_DATA (USER_ID, FULL_NAME, PASSWORD, STATUS_CD)
-        VALUES('medcoadmin', 'MedCo Admin', 'f8eb764674b57b5710e3c1665464e29', 'A');
+        VALUES('medcoadmin', 'MedCo Admin', '$DEFAULT_USER_PASSWORD_HASH', 'A');
     INSERT INTO i2b2pm.PM_USER_DATA (USER_ID, FULL_NAME, PASSWORD, STATUS_CD)
-        VALUES('medcouser', 'MedCo User', 'f8eb764674b57b5710e3c1665464e29', 'A');
+        VALUES('medcouser', 'MedCo User', '$DEFAULT_USER_PASSWORD_HASH', 'A');
     INSERT INTO i2b2pm.PM_USER_DATA (USER_ID, FULL_NAME, PASSWORD, STATUS_CD)
-        VALUES('medcoservice', 'MedCo Service User', '7cb1ac9deab165535494d60da1d3d7e', 'A');
+        VALUES('medcoservice', 'MedCo Service User', '$I2B2_SERVICE_PASSWORD_HASH', 'A');
 
     INSERT INTO i2b2pm.PM_PROJECT_USER_ROLES (PROJECT_ID, USER_ID, USER_ROLE_CD, STATUS_CD) VALUES
         ('MedCo', 'medcoservice', 'USER', 'A'),
