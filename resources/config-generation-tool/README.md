0504a736b82e34b3563dc3bc0e56b053599b5ce3
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-10-29 10:00:03 +0100

    Deleted some old resources files
    Former-commit-id: 3f47640a96f83d18bc2260090716dcab141d08a0 [formerly 27a45e61e7def16045c024318157f52889e9ed9b]
    Former-commit-id: fad6b3f82d67d3ff64206b368a9d9127b0812879

--- resources/config-generation-tool/README.md
+++
@@ -1,4 +0,0 @@
-# Configuration generation tool for MedCo deployment
-
-TODO: what is generated, how to use, CA is dev, how to gen CA,
---> go in medco-documentation
--- resources/utility-scripts/ubuntu_prereqs_setup.sh
+++ resources/utility-scripts/ubuntu_prereqs_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get -y update
-apt-get -y install git apt-transport-https ca-certificates curl software-properties-common screen default-jre nano unzip
+apt-get -y install git apt-transport-https curl software-properties-common screen
 
 apt-get remove docker docker-engine docker.io
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
