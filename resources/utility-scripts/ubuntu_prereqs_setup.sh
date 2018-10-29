78388b9e0135ad5ed2636e3213740216768c9122
Mickaël Misbach <mickael@misba.ch>
2018-10-29 15:16:09 +0100

    update docker compose version
    Former-commit-id: c8a13e9c6fa623095aa87ec966bf5069b918fd69 [formerly 8b41b1f4d01d97010a2fed3b337b649226f1dad5]
    Former-commit-id: 3f56f9fa48eda37a0c604f21bac15b9813bb8358

--- resources/utility-scripts/ubuntu_prereqs_setup.sh
+++ resources/utility-scripts/ubuntu_prereqs_setup.sh
@@ -20,6 +20,6 @@ apt-get -y install docker-ce postgresql-client-10
 
 docker run hello-world
 
-curl -L "https://github.com/docker/compose/releases/download/1.23.0-rc1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.23.0-rc3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/lca1/medco-deployment.git
