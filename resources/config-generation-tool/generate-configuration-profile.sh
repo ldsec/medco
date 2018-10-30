36e85552daf0b0b4fb7b49372375bdd98480ca4b
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-10-30 11:23:17 +0100

    Fixed config script
    Former-commit-id: 9286b74f20bf63ad4a60f137e480b75153cf640f [formerly 96b77599e116bbe56ca9acc0cccd1f33b2b76aee]
    Former-commit-id: fec31728a9eeaebdcd96408a60f217e25e0384df

--- resources/config-generation-tool/generate-configuration-profile.sh
+++ resources/config-generation-tool/generate-configuration-profile.sh
@@ -45,6 +45,6 @@ done
 echo "### Generating group.toml file"
 cat "$CONF_FOLDER"/srv*-public.toml > "$CONF_FOLDER/group.toml"
 "$CONF_FOLDER"/unlynxMedCo server getAggregateKey --file "$CONF_FOLDER/group.toml"
-"$CONF_FOLDER"/unlynxMedCo server generateDDTSecrets --file "$CONF_FOLDER/group.toml"
+"$CONF_FOLDER"/unlynxMedCo server generateTaggingSecrets --file "$CONF_FOLDER/group.toml"
 
 echo "### Configuration generated!"
