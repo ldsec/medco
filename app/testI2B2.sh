83a077c2304804862c980697a9aab2961a6676f5
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-10-30 10:58:40 +0100

    Changed test app scripts
    Former-commit-id: d12a560f79c04ba86cbf198a3bbfa81f704f4d9a

--- app/testI2B2.sh
+++ app/testI2B2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-./medco-loader -debug 2 v1 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entry 0 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml \
+./medco-loader -debug 2 v1 -g /opt/medco/medco-deployment/configuration-profiles/dev-3nodes-1host/group.toml --entry 0 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml \
 --dbHost localhost  --dbPort 5432 --dbName i2b2medcosrv0 --dbUser i2b2 --dbPassword i2b2
 
-./medco-loader -debug 2 v1 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entry 1 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml \
+./medco-loader -debug 2 v1 -g /home/jagomes/medco-deployment/configuration-profiles/dev-3nodes-1host/group.toml --entry 1 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml \
 --dbHost localhost  --dbPort 5432 --dbName i2b2medcosrv1 --dbUser i2b2 --dbPassword i2b2
 
-./medco-loader -debug 2 v1 -g /home/jagomes/medco-deployment/configuration-profiles/prod/3nodes-samehost/group.toml --entry 2 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml \
+./medco-loader -debug 2 v1 -g /home/jagomes/medco-deployment/configuration-profiles/dev-3nodes-1host/group.toml --entry 2 --sen ../data/i2b2/sensitive.txt -f ../data/i2b2/files.toml \
 --dbHost localhost  --dbPort 5432 --dbName i2b2medcosrv2 --dbUser i2b2 --dbPassword i2b2
