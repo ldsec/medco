74ced25ce5a8d64ca74b5e3831dfed9438e32c7e
Mickaël Misbach <mickael@misba.ch>
2018-11-27 19:53:03 +0100

    remove unnecessary SQL and add default data with e2e tests
    Former-commit-id: a2a327190c281053d67af9e5a44a11910d6e61d0 [formerly 1df062c286c15555b1bb386eec5f68a147db98c8]
    Former-commit-id: 8ca2668103e42d8e3f322a39432a76ac3e7e70a9

--- docker-images/i2b2-medco/sql/55-medco-pm-configuration.sh
+++ docker-images/i2b2-medco/sql/55-medco-pm-configuration.sh
@@ -5,6 +5,7 @@ set -Eeuo pipefail
 # generate password hashes
 I2B2_SERVICE_PASSWORD_HASH=$(java -classpath "$JBOSS_HOME/I2b2PasswordHash/" I2b2PasswordHash "$I2B2_SERVICE_PASSWORD")
 DEFAULT_USER_PASSWORD_HASH=$(java -classpath "$JBOSS_HOME/I2b2PasswordHash/" I2b2PasswordHash "$DEFAULT_USER_PASSWORD")
+E2E_USER_PASSWORD_HASH=$(java -classpath "$JBOSS_HOME/I2b2PasswordHash/" I2b2PasswordHash e2etest)
 
 psql $PSQL_PARAMS -d "$I2B2_DB_NAME" <<-EOSQL
     -- database lookups
@@ -16,6 +17,8 @@ psql $PSQL_PARAMS -d "$I2B2_DB_NAME" <<-EOSQL
     -- hive & users data
     insert into i2b2pm.pm_project_data (project_id, project_name, project_wiki, project_path, status_cd) values
         ('MedCo', 'MedCo', 'https://lca1.github.io/medco-documentation', '/MedCo', 'A');
+    insert into i2b2pm.pm_project_data (project_id, project_name, project_wiki, project_path, status_cd) values
+        ('e2etest', 'e2etest', 'https://lca1.github.io/medco-documentation', '/e2etest', 'A');
 
     -- cell URLs
     UPDATE i2b2pm.PM_CELL_DATA SET URL = 'http://i2b2-medco:8080/i2b2/services/QueryToolService/' WHERE CELL_ID = 'CRC';
@@ -30,17 +33,9 @@ psql $PSQL_PARAMS -d "$I2B2_DB_NAME" <<-EOSQL
     INSERT INTO i2b2pm.PM_USER_DATA (USER_ID, FULL_NAME, PASSWORD, STATUS_CD)
         VALUES('medcouser', 'MedCo User', '$DEFAULT_USER_PASSWORD_HASH', 'A');
     INSERT INTO i2b2pm.PM_USER_DATA (USER_ID, FULL_NAME, PASSWORD, STATUS_CD)
-        VALUES('medcoservice', 'MedCo Service User', '$I2B2_SERVICE_PASSWORD_HASH', 'A');
+        VALUES('e2etest', 'End-To-End Test User', '$E2E_USER_PASSWORD_HASH', 'A');
 
     INSERT INTO i2b2pm.PM_PROJECT_USER_ROLES (PROJECT_ID, USER_ID, USER_ROLE_CD, STATUS_CD) VALUES
-        ('MedCo', 'medcoservice', 'USER', 'A'),
-        ('MedCo', 'medcoservice', 'DATA_DEID', 'A'),
-        ('MedCo', 'medcoservice', 'DATA_OBFSC', 'A'),
-        ('MedCo', 'medcoservice', 'DATA_AGG', 'A'),
-        ('MedCo', 'medcoservice', 'DATA_LDS', 'A'),
-        ('MedCo', 'medcoservice', 'EDITOR', 'A'),
-        ('MedCo', 'medcoservice', 'DATA_PROT', 'A'),
-        ('MedCo', 'medcoservice', 'MANAGER', 'A'),
 
         ('MedCo', 'AGG_SERVICE_ACCOUNT', 'USER', 'A'),
         ('MedCo', 'AGG_SERVICE_ACCOUNT', 'MANAGER', 'A'),
@@ -59,12 +54,9 @@ psql $PSQL_PARAMS -d "$I2B2_DB_NAME" <<-EOSQL
         ('MedCo', 'medcouser', 'EDITOR', 'A'),
         ('MedCo', 'medcouser', 'DATA_PROT', 'A'),
 
-        -- give medcouser rights to Demo project
-        ('Demo', 'medcouser', 'USER', 'A'),
-        ('Demo', 'medcouser', 'DATA_DEID', 'A'),
-        ('Demo', 'medcouser', 'DATA_OBFSC', 'A'),
-        ('Demo', 'medcouser', 'DATA_AGG', 'A'),
-        ('Demo', 'medcouser', 'DATA_LDS', 'A'),
-        ('Demo', 'medcouser', 'EDITOR', 'A'),
-        ('Demo', 'medcouser', 'DATA_PROT', 'A');
+        -- e2e user
+        ('MedCo', 'e2etest', 'USER', 'A'),
+        ('MedCo', 'e2etest', 'DATA_OBFSC', 'A'),
+        ('MedCo', 'e2etest', 'DATA_AGG', 'A'),
+        ('MedCo', 'e2etest', 'DATA_LDS', 'A');
 EOSQL
--- docker-images/i2b2-medco/sql/65-medco-ontology.sh
+++ docker-images/i2b2-medco/sql/65-medco-ontology.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 
 psql $PSQL_PARAMS -d "$I2B2_DB_NAME" <<-EOSQL
 
-    -- table_access & root nodes
+    -- table_access
     CREATE TABLE medco_ont.table_access(
         C_TABLE_CD VARCHAR(50),
         C_TABLE_NAME VARCHAR(50),
@@ -30,26 +30,8 @@ psql $PSQL_PARAMS -d "$I2B2_DB_NAME" <<-EOSQL
         C_STATUS_CD CHAR(1),
         VALUETYPE_CD VARCHAR(50)
     );
-    --INSERT into medco_ont.table_access
-    --    ( C_TABLE_CD, C_TABLE_NAME, C_PROTECTED_ACCESS, C_HLEVEL, C_NAME, C_FULLNAME, C_SYNONYM_CD, C_VISUALATTRIBUTES,
-    --    C_TOOLTIP, C_FACTTABLECOLUMN, C_DIMTABLENAME, C_COLUMNNAME, C_COLUMNDATATYPE, C_DIMCODE, C_OPERATOR) values
-    --    ( 'MedCo', 'MedCo', 'N', 0, 'MedCo Ontology', '\medco\', 'N', 'CA', 'MedCo Ontology', 'concept_cd',
-    --    'concept_dimension', 'concept_path', 'T', '\medco\', 'LIKE');
-    insert into medco_ont.table_access (c_table_cd, c_table_name, c_protected_access, c_hlevel, c_fullname, c_name,
-        c_synonym_cd, c_visualattributes, c_facttablecolumn, c_dimtablename,
-        c_columnname, c_columndatatype, c_operator, c_dimcode, c_tooltip) VALUES
-        ('CLINICAL_SENSITIVE', 'CLINICAL_SENSITIVE', 'N', 2, '\medco\clinical\sensitive\', 'MedCo Clinical Sensitive Ontology',
-        'N', 'CA', 'concept_cd', 'concept_dimension', 'concept_path', 'T', 'LIKE', '\medco\clinical\sensitive\', 'MedCo Clinical Sensitive Ontology');
-    insert into medco_ont.table_access (c_table_cd, c_table_name, c_protected_access, c_hlevel, c_fullname, c_name,
-        c_synonym_cd, c_visualattributes, c_facttablecolumn, c_dimtablename,
-        c_columnname, c_columndatatype, c_operator, c_dimcode, c_tooltip) VALUES
-        ('CLINICAL_NON_SENSITIVE', 'CLINICAL_NON_SENSITIVE', 'N', 2, '\medco\clinical\nonsensitive\', 'MedCo Clinical Non-Sensitive Ontology',
-        'N', 'CA', 'concept_cd', 'concept_dimension', 'concept_path', 'T', 'LIKE', '\medco\clinical\nonsensitive\', 'MedCo Clinical Non-Sensitive Ontology');
-    insert into medco_ont.table_access (c_table_cd, c_table_name, c_protected_access, c_hlevel, c_fullname, c_name,
-        c_synonym_cd, c_visualattributes, c_facttablecolumn, c_dimtablename,
-        c_columnname, c_columndatatype, c_operator, c_dimcode, c_tooltip) VALUES
-        ('GENOMIC', 'GENOMIC', 'N', 1, '\medco\genomic\', 'MedCo Genomic Ontology',
-        'N', 'CA', 'concept_cd', 'concept_dimension', 'concept_path', 'T', 'LIKE', '\medco\genomic\', 'MedCo Genomic Ontology');
+    ALTER TABLE medco_ont.table_access OWNER TO $I2B2_DB_USER;
+
     insert into medco_ont.table_access (c_table_cd, c_table_name, c_protected_access, c_hlevel, c_fullname, c_name,
         c_synonym_cd, c_visualattributes, c_facttablecolumn, c_dimtablename,
         c_columnname, c_columndatatype, c_operator, c_dimcode, c_tooltip) VALUES
@@ -63,113 +45,13 @@ psql $PSQL_PARAMS -d "$I2B2_DB_NAME" <<-EOSQL
         C_DESCRIPTION VARCHAR(100),
         CONSTRAINT SCHEMES_PK PRIMARY KEY (C_KEY)
     );
+    ALTER TABLE medco_ont.schemes OWNER TO $I2B2_DB_USER;
+
+    -- todo: revise those schemes
     insert into medco_ont.schemes(c_key, c_name, c_description) values('TAG_ID:', 'TAG_ID', 'MedCo tag identifier');
     insert into medco_ont.schemes(c_key, c_name, c_description) values('ENC_ID:', 'ENC_ID', 'MedCo sensitive concept identifier (to be encrypted)');
     insert into medco_ont.schemes(c_key, c_name, c_description) values('CLEAR:', 'CLEAR', 'MedCo clear value');
     insert into medco_ont.schemes(c_key, c_name, c_description) values('GEN:', 'GEN', 'MedCo genomic annotations');
-
-    -- ontology table
-    CREATE TABLE medco_ont.shrine(
-        C_HLEVEL NUMERIC(22,0),
-        C_FULLNAME VARCHAR(900),
-        C_NAME VARCHAR(2000),
-        C_SYNONYM_CD CHAR(1),
-        C_VISUALATTRIBUTES CHAR(3),
-        C_TOTALNUM NUMERIC(22,0),
-        C_BASECODE VARCHAR(450),
-        C_METADATAXML TEXT,
-        C_FACTTABLECOLUMN VARCHAR(50),
-        C_TABLENAME VARCHAR(50),
-        C_COLUMNNAME VARCHAR(50),
-        C_COLUMNDATATYPE VARCHAR(50),
-        C_OPERATOR VARCHAR(10),
-        C_DIMCODE VARCHAR(900),
-        C_COMMENT TEXT,
-        C_TOOLTIP VARCHAR(900),
-        UPDATE_DATE DATE,
-        DOWNLOAD_DATE DATE,
-        IMPORT_DATE DATE,
-        SOURCESYSTEM_CD VARCHAR(50),
-        VALUETYPE_CD VARCHAR(50),
-        M_APPLIED_PATH VARCHAR(900),
-        M_EXCLUSION_CD VARCHAR(900)
-    );
-
-    -- clinical sensitive ontology
-    CREATE TABLE medco_ont.clinical_sensitive(
-        c_hlevel numeric(22,0) not null,
-        c_fullname character varying(900) not null,
-        c_name character varying(2000) not null,
-        c_synonym_cd character(1) not null,
-        c_visualattributes character(3) not null,
-        c_totalnum numeric(22,0),
-        c_basecode character varying(450),
-        c_metadataxml text,
-        c_facttablecolumn character varying(50) not null,
-        c_tablename character varying(50) not null,
-        c_columnname character varying(50) not null,
-        c_columndatatype character varying(50) not null,
-        c_operator character varying(10) not null,
-        c_dimcode character varying(900) not null,
-        c_comment text,
-        c_tooltip character varying(900),
-        update_date date not null,
-        download_date date,
-        import_date date,
-        sourcesystem_cd character varying(50),
-        valuetype_cd character varying(50),
-        m_applied_path character varying(900) not null,
-        m_exclusion_cd character varying(900),
-        c_path character varying(700),
-        c_symbol character varying(50),
-        pcori_basecode character varying(50)
-    );
-    ALTER TABLE ONLY medco_ont.clinical_sensitive ADD CONSTRAINT fullname_pk_20 PRIMARY KEY (c_fullname);
-    ALTER TABLE ONLY medco_ont.clinical_sensitive ADD CONSTRAINT basecode_un_20 UNIQUE (c_basecode);
-    --insert into medco_ont.clinical_sensitive (c_hlevel, c_fullname, c_name, c_synonym_cd, c_visualattributes, c_totalnum,
-    --    c_facttablecolumn, c_tablename, c_columnname, c_columndatatype, c_operator, c_dimcode, c_comment, c_tooltip, update_date,
-    --    download_date, import_date, valuetype_cd, m_applied_path) values
-    --    ('2', '\medco\clinical\sensitive\', 'MedCo Clinical Sensitive Ontology', 'N', 'CA', '0', 'concept_cd', 'concept_dimension', 'concept_path',
-    --    'T', 'LIKE', '\medco\clinical\sensitive\', 'MedCo Clinical Sensitive Ontology', '\medco\clinical\sensitive\',
-    --    'NOW()', 'NOW()', 'NOW()', 'ENC_ID', '@');
-
-    -- clinical non-sensitive ontology
-    CREATE TABLE medco_ont.clinical_non_sensitive(
-        c_hlevel numeric(22,0) not null,
-        c_fullname character varying(900) not null,
-        c_name character varying(2000) not null,
-        c_synonym_cd character(1) not null,
-        c_visualattributes character(3) not null,
-        c_totalnum numeric(22,0),
-        c_basecode character varying(450),
-        c_metadataxml text,
-        c_facttablecolumn character varying(50) not null,
-        c_tablename character varying(50) not null,
-        c_columnname character varying(50) not null,
-        c_columndatatype character varying(50) not null,
-        c_operator character varying(10) not null,
-        c_dimcode character varying(900) not null,
-        c_comment text,
-        c_tooltip character varying(900),
-        update_date date not null,
-        download_date date,
-        import_date date,
-        sourcesystem_cd character varying(50),
-        valuetype_cd character varying(50),
-        m_applied_path character varying(900) not null,
-        m_exclusion_cd character varying(900),
-        c_path character varying(700),
-        c_symbol character varying(50),
-        pcori_basecode character varying(50)
-    );
-    ALTER TABLE ONLY medco_ont.clinical_non_sensitive ADD CONSTRAINT fullname_pk_21 PRIMARY KEY (c_fullname);
-    ALTER TABLE ONLY medco_ont.clinical_non_sensitive ADD CONSTRAINT basecode_un_21 UNIQUE (c_basecode);
-    --insert into medco_ont.clinical_non_sensitive (c_hlevel, c_fullname, c_name, c_synonym_cd, c_visualattributes, c_totalnum,
-    --    c_facttablecolumn, c_tablename, c_columnname, c_columndatatype, c_operator, c_dimcode, c_comment, c_tooltip, update_date,
-    --    download_date, import_date, valuetype_cd, m_applied_path) values
-    --    ('2', '\medco\clinical\nonsensitive\', 'MedCo Clinical Non-Sensitive Ontology', 'N', 'CA', '0', 'concept_cd', 'concept_dimension', 'concept_path',
-    --    'T', 'LIKE', '\medco\clinical\nonsensitive\', 'MedCo Clinical Non-Sensitive Ontology', '\medco\clinical\nonsensitive\',
-    --    'NOW()', 'NOW()', 'NOW()', 'CLEAR', '@');
 
     -- tagged sensitive ontology
     CREATE TABLE medco_ont.sensitive_tagged (
@@ -200,64 +82,16 @@ psql $PSQL_PARAMS -d "$I2B2_DB_NAME" <<-EOSQL
         c_symbol character varying(50),
         pcori_basecode character varying(50)
     );
-    ALTER TABLE ONLY medco_ont.sensitive_tagged ADD CONSTRAINT fullname_pk_11 PRIMARY KEY (c_fullname);
-    ALTER TABLE ONLY medco_ont.sensitive_tagged ADD CONSTRAINT basecode_un_11 UNIQUE (c_basecode);
-    --insert into medco_ont.sensitive_tagged (c_hlevel, c_fullname, c_name, c_synonym_cd, c_visualattributes, c_totalnum,
-    --c_facttablecolumn, c_tablename, c_columnname, c_columndatatype, c_operator, c_dimcode, c_comment, c_tooltip, update_date,
-    --download_date, import_date, valuetype_cd, m_applied_path) values
-    --('1', '\medco\tagged\', 'MedCo Sensitive Tagged Ontology', 'N', 'CA', '0', 'concept_cd', 'concept_dimension', 'concept_path',
-    --'T', 'LIKE', '\medco\tagged\', 'MedCo Sensitive Tagged Ontology', '\medco\tagged\', 'NOW()', 'NOW()', 'NOW()', 'TAG_ID', '@');
-
-    -- genomic ontology
-    CREATE TABLE medco_ont.genomic(
-        c_hlevel numeric(22,0) not null,
-        c_fullname character varying(900) not null,
-        c_name character varying(2000) not null,
-        c_synonym_cd character(1) not null,
-        c_visualattributes character(3) not null,
-        c_totalnum numeric(22,0),
-        c_basecode character varying(450),
-        c_metadataxml text,
-        c_facttablecolumn character varying(50) not null,
-        c_tablename character varying(50) not null,
-        c_columnname character varying(50) not null,
-        c_columndatatype character varying(50) not null,
-        c_operator character varying(10) not null,
-        c_dimcode character varying(900) not null,
-        c_comment text,
-        c_tooltip character varying(900),
-        update_date date not null,
-        download_date date,
-        import_date date,
-        sourcesystem_cd character varying(50),
-        valuetype_cd character varying(50),
-        m_applied_path character varying(900) not null,
-        m_exclusion_cd character varying(900),
-        c_path character varying(700),
-        c_symbol character varying(50),
-        pcori_basecode character varying(50)
-    );
-    ALTER TABLE ONLY medco_ont.genomic ADD CONSTRAINT fullname_pk_22 PRIMARY KEY (c_fullname);
-    ALTER TABLE ONLY medco_ont.genomic ADD CONSTRAINT basecode_un_22 UNIQUE (c_basecode);
-    insert into medco_ont.genomic (c_hlevel, c_fullname, c_name, c_synonym_cd, c_visualattributes, c_totalnum,
-        c_facttablecolumn, c_tablename, c_columnname, c_columndatatype, c_operator, c_dimcode, c_comment, c_tooltip, update_date,
-        download_date, import_date, valuetype_cd, m_applied_path) values
-        ('1', '\medco\genomic\', 'MedCo Genomic Ontology', 'N', 'CA', '0', 'concept_cd', 'concept_dimension', 'concept_path',
-        'T', 'LIKE', '\medco\genomic\', 'MedCo Genomic Ontology', '\medco\genomic\',
-        'NOW()', 'NOW()', 'NOW()', 'GEN', '@');
+    ALTER TABLE medco_ont.sensitive_tagged OWNER TO $I2B2_DB_USER;
+    CREATE INDEX META_FULLNAME_IDX_sensitive_tagged ON medco_ont.sensitive_tagged(C_FULLNAME);
+    CREATE INDEX META_APPLIED_PATH_IDX_sensitive_tagged ON medco_ont.sensitive_tagged(M_APPLIED_PATH);
+    CREATE INDEX META_EXCLUSION_IDX_sensitive_tagged ON medco_ont.sensitive_tagged(M_EXCLUSION_CD);
+    CREATE INDEX META_HLEVEL_IDX_sensitive_tagged ON medco_ont.sensitive_tagged(C_HLEVEL);
+    CREATE INDEX META_SYNONYM_IDX_sensitive_tagged ON medco_ont.sensitive_tagged(C_SYNONYM_CD);
 
     -- permissions
-    ALTER TABLE medco_ont.table_access OWNER TO $I2B2_DB_USER;
-    ALTER TABLE medco_ont.schemes OWNER TO $I2B2_DB_USER;
-    ALTER TABLE medco_ont.shrine OWNER TO $I2B2_DB_USER;
-    ALTER TABLE medco_ont.genomic OWNER TO $I2B2_DB_USER;
-    ALTER TABLE medco_ont.clinical_sensitive OWNER TO $I2B2_DB_USER;
-    ALTER TABLE medco_ont.clinical_non_sensitive OWNER TO $I2B2_DB_USER;
-    ALTER TABLE medco_ont.sensitive_tagged OWNER TO $I2B2_DB_USER;
-
     grant all on schema medco_ont to $I2B2_DB_USER;
     grant all privileges on all tables in schema medco_ont to $I2B2_DB_USER;
     grant all privileges on all sequences in schema medco_ont to $I2B2_DB_USER;
     grant all privileges on all functions in schema medco_ont to $I2B2_DB_USER;
-
 EOSQL
