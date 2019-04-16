7eb2d6069fdb6375c1bef5cc5b8999acdc9d876f
Mickaël Misbach <mickael@misba.ch>
2019-04-16 20:21:08 +0200

    add configuration options; linting

--- util/configuration.go
+++ util/configuration.go
@@ -7,49 +7,66 @@ import (
 	"strconv"
 )
 
-// LogLevel returns the log level, assuming the same convention as the cothority / unlynx log levels:
+// LogLevel is the log level, assuming the same convention as the cothority / unlynx log levels:
 // TRACE(5), DEBUG(4), INFO(3), WARNING(2), ERROR(1), FATAL(0)
 var LogLevel int
 
+// UnlynxGroupFilePath is the path of the unlynx group file from which is derived the cothority public key
 var UnlynxGroupFilePath string
+// UnlynxGroupFileIdx is the index (in the group file) of this node
 var UnlynxGroupFileIdx int
+// UnlynxTimeoutSeconds is the unlynx communication timeout (seconds)
 var UnlynxTimeoutSeconds int
 
-// URL of the i2b2 hive this connector is using
+// I2b2HiveURL is the URL of the i2b2 hive this connector is using
 var I2b2HiveURL string
-// i2b2 login domain
+// I2b2LoginDomain is the i2b2 login domain
 var I2b2LoginDomain string
-// i2b2 login project
+// I2b2LoginProject is the i2b2 login project
 var I2b2LoginProject string
-// i2b2 login user
+// I2b2LoginUser is the i2b2 login user
 var I2b2LoginUser string
-// i2b2 login password
+// I2b2LoginPassword is the i2b2 login password
 var I2b2LoginPassword string
-// i2b2 timeout (seconds)
+// I2b2TimeoutSeconds is the i2b2 timeout (seconds)
 var I2b2TimeoutSeconds int
 
-// token (shared secret) used for internal PICSURE 2 authorization
-var picsure2InternalToken string
+// JwksURL is the URL from which the JWT signing keys are retrieved
+var JwksURL string
+// JwksTTLSeconds is the TTL of JWKS requests
+var JwksTTLSeconds int64
+// OidcJwtIssuer is the token issuer (for JWT validation)
+var OidcJwtIssuer string
+// OidcClientID is the OIDC client ID
+var OidcClientID string
+// OidcJwtUserIDClaim is the JWT claim containing the user ID
+var OidcJwtUserIDClaim string
+
 
 func init() {
 	SetLogLevel(os.Getenv("LOG_LEVEL"))
 
 	UnlynxGroupFilePath = os.Getenv("UNLYNX_GROUP_FILE_PATH")
-	UnlynxTimeoutSeconds = 180
+	UnlynxTimeoutSeconds = 3 * 60 // 3 minutes
 
-	UnlynxGroupFileIdx, err := strconv.ParseInt(os.Getenv("UNLYNX_GROUP_FILE_IDX"), 10, 64)
+	idx, err := strconv.ParseInt(os.Getenv("UNLYNX_GROUP_FILE_IDX"), 10, 64)
 	if err != nil || UnlynxGroupFileIdx < 0 {
 		logrus.Warn("invalid UnlynxGroupFileIdx")
 	}
+	UnlynxGroupFileIdx = int(idx)
 
 	I2b2HiveURL = os.Getenv("I2B2_HIVE_URL")
 	I2b2LoginDomain = os.Getenv("I2B2_LOGIN_DOMAIN")
 	I2b2LoginProject = os.Getenv("I2B2_LOGIN_PROJECT")
 	I2b2LoginUser = os.Getenv("I2B2_LOGIN_USER")
 	I2b2LoginPassword = os.Getenv("I2B2_LOGIN_PASSWORD")
-	I2b2TimeoutSeconds = 180
+	I2b2TimeoutSeconds = 3 * 60 // 3 minutes
 
-	picsure2InternalToken = os.Getenv("PICSURE2_INTERNAL_TOKEN")
+	JwksURL = os.Getenv("JWKS_URL")
+	JwksTTLSeconds = 60 * 60 // 1 hour
+	OidcJwtIssuer = os.Getenv("OIDC_JWT_ISSUER")
+	OidcClientID = os.Getenv("OIDC_CLIENT_ID")
+	OidcJwtUserIDClaim = os.Getenv("OIDC_JWT_USER_ID_CLAIM")
 }
 
 // SetLogLevel initializes the log levels of all loggers
