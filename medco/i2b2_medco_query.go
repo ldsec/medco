8cc7b514d218a8f79f29be306a2357c4ef891716
Mickaël Misbach <mickael@misba.ch>
2019-04-16 20:26:20 +0200

    boolean bug fix; linting

--- medco/i2b2_medco_query.go
+++ medco/i2b2_medco_query.go
@@ -6,25 +6,22 @@ import (
 	"github.com/lca1/medco-connector/swagger/models"
 	"github.com/lca1/medco-connector/unlynx"
 	"github.com/sirupsen/logrus"
-	"time"
 )
 
-// handle i2b2MedCo query
-func I2b2MedCoQuery(query *models.QueryI2b2Medco) (result *models.QueryResultElement, err error) {
+// I2b2MedCoQuery executes an i2b2-medco query
+func I2b2MedCoQuery(queryName string, query *models.QueryI2b2Medco) (result *models.QueryResultElement, err error) {
 	// todo: timers
 	// todo: log query (with associated status)
 	// todo: query types (agregated per site, obfuscated per site, aggregated total)
 	// todo: query type: patient list
 	// todo: put user + query type + unique ID in query name
 
-	queryName := "MedCo query (" + time.Now().Format(time.RFC3339Nano) + ")"
-
 	// tag query terms
 	taggedQueryTerms, err := unlynx.GetQueryTermsDDT(queryName, extractEncryptedQueryTerms(query.Panels))
 	if err != nil {
 		return
 	}
-	logrus.Info(queryName, "tagged", len(taggedQueryTerms), "elements with unlynx")
+	logrus.Info(queryName, ": tagged ", len(taggedQueryTerms), " elements with unlynx")
 
 	// i2b2 PSM query with tagged items
 	panelsItemKeys, panelsIsNot, err := prepareI2b2PsmQuery(query.Panels, taggedQueryTerms)
@@ -32,18 +29,18 @@ func I2b2MedCoQuery(query *models.QueryI2b2Medco) (result *models.QueryResultEle
 		return
 	}
 
-	patientCount, patientSetId, err := i2b2.ExecutePsmQuery(queryName, panelsItemKeys, panelsIsNot)
+	patientCount, patientSetID, err := i2b2.ExecutePsmQuery(queryName, panelsItemKeys, panelsIsNot)
 	if err != nil {
 		return
 	}
-	logrus.Info(queryName, "got", patientCount, "in patient set", patientSetId, "with i2b2")
+	logrus.Info(queryName, ": got ", patientCount, " in patient set ", patientSetID, " with i2b2")
 
 	// i2b2 PDO query to get the dummy flags
-	patientIDs, patientDummyFlags, err := i2b2.GetPatientSet(patientSetId)
+	patientIDs, patientDummyFlags, err := i2b2.GetPatientSet(patientSetID)
 	if err != nil {
 		return
 	}
-	logrus.Info(queryName, "got", len(patientIDs), "patient IDs and", len(patientDummyFlags), "dummy flags with i2b2")
+	logrus.Info(queryName, ": got ", len(patientIDs), " patient IDs and ", len(patientDummyFlags), " dummy flags with i2b2")
 
 	// aggregate and key-switch the result
 	encCount, err := unlynx.AggregateAndKeySwitchDummyFlags(queryName, patientDummyFlags, query.UserPublicKey)
@@ -59,7 +56,7 @@ func I2b2MedCoQuery(query *models.QueryI2b2Medco) (result *models.QueryResultEle
 func extractEncryptedQueryTerms(panels []*models.QueryI2b2MedcoPanelsItems0) (encQueryTerms []string) {
 	for _, panel := range panels {
 		for _, item := range panel.Items {
-			if item.Encrypted {
+			if *item.Encrypted {
 				encQueryTerms = append(encQueryTerms, item.QueryTerm)
 			}
 		}
@@ -69,11 +66,12 @@ func extractEncryptedQueryTerms(panels []*models.QueryI2b2MedcoPanelsItems0) (en
 
 func prepareI2b2PsmQuery(panels []*models.QueryI2b2MedcoPanelsItems0, taggedQueryTerms map[string]string) (panelsItemKeys [][]string, panelsIsNot []bool, err error) {
 	for panelIdx, panel := range panels {
-		panelsIsNot = append(panelsIsNot, panel.Not)
+		panelsIsNot = append(panelsIsNot, *panel.Not)
 
+		panelsItemKeys = append(panelsItemKeys, []string{})
 		for _, item := range panel.Items {
 			var itemKey string
-			if item.Encrypted {
+			if *item.Encrypted {
 
 				if tag, ok := taggedQueryTerms[item.QueryTerm]; ok {
 					itemKey = `\\SENSITIVE_TAGGED\medco\tagged\` + tag + `\`
