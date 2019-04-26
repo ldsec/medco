733e8ab56b051720677127947e77d73a50c4e162
Mickaël Misbach <mickael@misba.ch>
2019-04-26 11:02:14 +0200

    update test-network generation scripts for v0.2.0
    Former-commit-id: e645420aa999d446fff08afed0e328f507ca210f [formerly a53e27f3023a7fffd07338199f0858c90cc09da6]
    Former-commit-id: 4c0276b452996e50cc940f5700d7bba766a1dac5

--- resources/profile-generation-scripts/test-network/step1.sh
+++ resources/profile-generation-scripts/test-network/step1.sh
@@ -2,38 +2,46 @@
 set -Eeuo pipefail
 shopt -s nullglob
 
+# command-line arguments
 if [[ $# -ne 3 ]]; then
-    echo "Wrong number of arguments, usage: bash $0 <profile_name> <node index> <node DNS>"
+    echo "Wrong number of arguments, usage: bash $0 <network_name> <node index> <node DNS name>"
     exit 1
 fi
-
+NETWORK_NAME="$1"
 NODE_IDX="$2"
-NODE_DNS="$3"
-PROFILE_NAME="test-network-$1-node$NODE_IDX"
+NODE_DNS_NAME="$3"
 
+# convenience variables
+PROFILE_NAME="test-network-$NETWORK_NAME-node$NODE_IDX"
 SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MEDCO_UNLYNX_VER="v0.2.0-rc1"
 CONF_FOLDER="$SCRIPT_FOLDER/../../../configuration-profiles/$PROFILE_NAME"
 COMPOSE_FOLDER="$SCRIPT_FOLDER/../../../compose-profiles/$PROFILE_NAME"
-
 if [[ -d ${CONF_FOLDER} ]] || [[ -d ${COMPOSE_FOLDER} ]]; then
-    echo "The compose or configuration profile folder exists. Aborting."
+    echo "The compose and/or configuration profile folder exists. Aborting."
     exit 2
 fi
 
-read -p "### About to generate configuration of node $NODE_IDX ($NODE_DNS) for profile $PROFILE_NAME, <Enter> to continue, <Ctrl+C> to abort."
+read -p "### About to generate configuration of node $NODE_IDX ($NODE_DNS_NAME) for profile $PROFILE_NAME, <Enter> to continue, <Ctrl+C> to abort."
 
 echo "### Dependencies check, script will abort if dependency if not found"
 which docker openssl
 
+# ===================== pre-requisites ======================
 mkdir "$CONF_FOLDER" "$COMPOSE_FOLDER"
+echo -n "$NODE_DNS_NAME" > "$CONF_FOLDER/srv$NODE_IDX-nodednsname.txt"
 
+
+# ===================== unlynx keys =========================
 echo "### Generating unlynx keys"
-docker run -v "$CONF_FOLDER":/medco-configuration -u $(id -u):$(id -g) medco/medco-unlynx \
-    server setupNonInteractive --serverBinding "$NODE_DNS:2000" --description "${PROFILE_NAME}_medco_unlynx_server" \
+docker run -v "$CONF_FOLDER":/medco-configuration -u $(id -u):$(id -g) medco/medco-unlynx:${MEDCO_UNLYNX_VER} \
+    server setupNonInteractive --serverBinding "$NODE_DNS_NAME:2000" --description "${PROFILE_NAME}_medco_unlynx_server" \
     --privateTomlPath "/medco-configuration/srv$NODE_IDX-private.toml" \
     --publicTomlPath "/medco-configuration/srv$NODE_IDX-public.toml"
 echo "### Unlynx keys generated!"
 
+
+# ===================== HTTPS cert ==========================
 echo "### Generating self-signed HTTPS certificate"
 cat > "$SCRIPT_FOLDER/openssl.cnf" <<EOF
 [req]
@@ -49,7 +57,7 @@ localityName = Lausanne
 localityName_default = Lausanne
 organizationalUnitName = EPFL LCA1
 organizationalUnitName_default = EPFL LCA1
-commonName = ${NODE_DNS}
+commonName = ${NODE_DNS_NAME}
 commonName_max = 64
 
 [ v3_req ]
@@ -58,7 +66,7 @@ keyUsage = nonRepudiation, digitalSignature, keyEncipherment
 subjectAltName = @alt_names
 
 [alt_names]
-DNS.1 = ${NODE_DNS}
+DNS.1 = ${NODE_DNS_NAME}
 EOF
 
 openssl genrsa -out "$CONF_FOLDER/certificate.key" 2048
@@ -70,16 +78,41 @@ cp "$CONF_FOLDER/certificate.crt" "$CONF_FOLDER/srv$NODE_IDX-certificate.crt"
 rm "$SCRIPT_FOLDER/openssl.cnf"
 echo "### Certificate generated!"
 
+
+# ===================== compose profile =====================
 echo "### Generating compose profile"
-cp "$SCRIPT_FOLDER/docker-compose.yml" "$COMPOSE_FOLDER/"
+cp "$SCRIPT_FOLDER/docker-compose.common.yml" "$SCRIPT_FOLDER/docker-compose.node.yml" "$COMPOSE_FOLDER/"
 cat > "$COMPOSE_FOLDER/.env" <<EOF
-MEDCO_NODE_URL=https://${NODE_DNS}
+MEDCO_NODE_URL=https://${NODE_DNS_NAME}
 MEDCO_NODE_IDX=${NODE_IDX}
 MEDCO_PROFILE_NAME=${PROFILE_NAME}
+MEDCO_NETWORK_NAME=${NETWORK_NAME}
+
+I2B2_WILDFLY_PASSWORD=admin
+I2B2_SERVICE_PASSWORD=pFjy3EjDVwLfT2rB9xkK
+I2B2_USER_PASSWORD=demouser
+POSTGRES_PASSWORD=postgres1
+PGADMIN_DEFAULT_PASSWORD=admin
+
+KEYCLOAK_CLIENT_ID=medco
+KEYCLOAK_USER_CLAIM=preferred_username
+
 EOF
+
+if [[ ${NODE_IDX} -eq 0 ]]; then
+    echo "### Generating additional configuration for central node"
+    cp "$SCRIPT_FOLDER/docker-compose.central.yml" "$COMPOSE_FOLDER/"
+    cat >> "$COMPOSE_FOLDER/.env" <<EOF
+KEYCLOAK_PASSWORD=keycloak
+
+EOF
+fi
 echo "### Compose profile generated!"
 
+
+# ===================== public archive ======================
 echo "### Generating archive to be shared"
 tar czvf "$CONF_FOLDER/srv$NODE_IDX-public.tar.gz" -C "$CONF_FOLDER" \
-    "srv$NODE_IDX-certificate.crt" "srv$NODE_IDX-public.toml"
+    "srv$NODE_IDX-certificate.crt" "srv$NODE_IDX-public.toml" "srv$NODE_IDX-nodednsname.txt"
 echo "### DONE! srv$NODE_IDX-public.tar.gz generated, ready for step 2"
+
--- resources/profile-generation-scripts/test-network/step2.sh
+++ resources/profile-generation-scripts/test-network/step2.sh
@@ -2,35 +2,59 @@
 set -Eeuo pipefail
 shopt -s nullglob
 
+# command-line arguments
 if [[ $# -ne 2 ]]; then
     echo "Wrong number of arguments, usage: bash $0 <profile_name> <node index>"
     exit 1
 fi
-
+NETWORK_NAME="$1"
 NODE_IDX="$2"
-PROFILE_NAME="test-network-$1-node$NODE_IDX"
 
+# convenience variables
+PROFILE_NAME="test-network-$NETWORK_NAME-node$NODE_IDX"
 SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MEDCO_UNLYNX_VER="v0.2.0-rc1"
 CONF_FOLDER="$SCRIPT_FOLDER/../../../configuration-profiles/$PROFILE_NAME"
 COMPOSE_FOLDER="$SCRIPT_FOLDER/../../../compose-profiles/$PROFILE_NAME"
-
-if [[ ! -d ${CONF_FOLDER} ]] || [[ ! -d ${COMPOSE_FOLDER} ]] || [[ -f ${CONF_FOLDER/group.toml} ]]; then
+if [[ ! -d ${CONF_FOLDER} ]] || [[ ! -d ${COMPOSE_FOLDER} ]] || [[ -f ${CONF_FOLDER}/group.toml ]]; then
     echo "The compose or configuration profile folder does not exist, or the step 2 has already been executed. Aborting."
     exit 2
 fi
 
 read -p "### About to finalize the configuration of node $NODE_IDX for profile $PROFILE_NAME, <Enter> to continue, <Ctrl+C> to abort."
 
+# ===================== archive extraction ==================
 echo "### Extracting archives from other nodes"
 pushd "${CONF_FOLDER}"
-for archive in $(ls -1 *-public.tar.gz); do tar -zxvf ${archive}; done
+for archive in $(ls -1 srv*-public.tar.gz); do tar -zxvf ${archive}; done
 popd
 echo "### Archives extracted"
 
+
+# ===================== unlynx group key ====================
 echo "### Generating group.toml file"
 cat "$CONF_FOLDER"/srv*-public.toml > "$CONF_FOLDER/group.toml"
-docker run -v "$CONF_FOLDER":/medco-configuration -u $(id -u):$(id -g) medco/medco-unlynx \
+docker run -v "$CONF_FOLDER":/medco-configuration -u $(id -u):$(id -g) medco/medco-unlynx:${MEDCO_UNLYNX_VER} \
     server getAggregateKey --file "/medco-configuration/group.toml"
-docker run -v "$CONF_FOLDER":/medco-configuration -u $(id -u):$(id -g) medco/medco-unlynx \
+docker run -v "$CONF_FOLDER":/medco-configuration -u $(id -u):$(id -g) medco/medco-unlynx:${MEDCO_UNLYNX_VER} \
     server generateTaggingSecrets --file "/medco-configuration/group.toml"
+echo "### group.toml file generated"
+
+
+# ===================== compose profile =====================
+echo "### Updating compose profile"
+declare -a MEDCO_NODES_NAME MEDCO_NODES_CONNECTOR_URL
+pushd "${CONF_FOLDER}"
+for nodednsname in $(ls -1 srv*-nodednsname.txt); do
+    MEDCO_NODES_NAME+=("$(<${nodednsname})")
+    MEDCO_NODES_CONNECTOR_URL+=("https://$(<${nodednsname})/medco-connector/picsure2")
+done
+popd
+
+export IFS=,
+cat >> "$COMPOSE_FOLDER/.env" <<EOF
+KEYCLOAK_REALM_URL=https://$(<${CONF_FOLDER}/srv0-nodednsname.txt)/auth/realms/master
+MEDCO_NODES_NAME=${MEDCO_NODES_NAME[*]}
+MEDCO_NODES_CONNECTOR_URL=${MEDCO_NODES_CONNECTOR_URL[*]}
+EOF
 echo "### DONE! MedCo profile ready"
