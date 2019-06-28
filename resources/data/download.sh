37035d2185d657211abd90d486563c3fff9fb142
Mickaël Misbach <mickael@misba.ch>
2019-06-28 16:09:11 +0200

    add data folder and download script
    Former-commit-id: e767b30d976a6ba8c6546b164daf90a48e608005 [formerly 0ff8818670a742b4bf50b4a0f0a588acf2392dad]
    Former-commit-id: a2b760cfbb44788219946cfbc56058f7afef3d92

---
+++ resources/data/download.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+### script to download the MedCo example datasets
+
+SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_URL="https://github.com/lca1/medco-loader/blob/master"
+
+# genomic (v0) dataset
+mkdir -p ${SCRIPT_FOLDER}/genomic/tcga_cbio/
+wget -O ${SCRIPT_FOLDER}/genomic/tcga_cbio/mutation_data.csv ${REPO_URL}/data/genomic/tcga_cbio/mutation_data.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/genomic/tcga_cbio/clinical_data.csv ${REPO_URL}/data/genomic/tcga_cbio/clinical_data.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/genomic/sensitive.txt ${REPO_URL}/data/genomic/sensitive.txt?raw=true
+
+# i2b2 demo (v1) dataset
+mkdir -p ${SCRIPT_FOLDER}/i2b2/original/
+mkdir -p ${SCRIPT_FOLDER}/i2b2/converted/
+wget -O ${SCRIPT_FOLDER}/i2b2/original/birn.csv ${REPO_URL}/data/i2b2/original/birn.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/i2b2/original/concept_dimension.csv ${REPO_URL}/data/i2b2/original/concept_dimension.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/i2b2/original/custom_meta.csv ${REPO_URL}/data/i2b2/original/custom_meta.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/i2b2/original/dummy_to_patient.csv ${REPO_URL}/data/i2b2/original/dummy_to_patient.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/i2b2/original/i2b2.csv ${REPO_URL}/data/i2b2/original/i2b2.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/i2b2/original/icd10_icd9.csv ${REPO_URL}/data/i2b2/original/icd10_icd9.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/i2b2/original/observation_fact.csv ${REPO_URL}/data/i2b2/original/observation_fact.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/i2b2/original/patient_dimension.csv ${REPO_URL}/data/i2b2/original/patient_dimension.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/i2b2/original/table_access.csv ${REPO_URL}/data/i2b2/original/table_access.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/i2b2/original/visit_dimension.csv ${REPO_URL}/data/i2b2/original/visit_dimension.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/i2b2/original/concept_dimension.csv ${REPO_URL}/data/i2b2/original/concept_dimension.csv?raw=true
+wget -O ${SCRIPT_FOLDER}/i2b2/sensitive.txt ${REPO_URL}/data/i2b2/sensitive.txt?raw=true
+wget -O ${SCRIPT_FOLDER}/i2b2/files.toml ${REPO_URL}/data/i2b2/files.toml?raw=true
