10e5eb83b4fd40d285472538497d0ef1e76c0b36
Mickaël Misbach <mickael@misba.ch>
2020-10-20 13:51:19 +0200

    add default value for version

--- version.go
+++ version.go
@@ -2,4 +2,4 @@ package medco
 
 // Version specifies the version of the code, it may be set at build time with:
 // -ldflags "-X github.com/ldsec/medco.Version=xxx"
-var Version string
+var Version = "dev"
