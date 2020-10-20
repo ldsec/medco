f1e11715c2ba886748152eb24dce56cef31f5386
Nicolas Freundler <nicolas.freundler@protonmail.ch>
2020-10-20 12:48:13 +0200

    changes in CSV file mode. string 0777 was interpreted as 511

--- connector/util/client/result_csv.go
+++ connector/util/client/result_csv.go
@@ -11,7 +11,6 @@ import (
 
 const comma rune = ','
 const useCarriageReturnThenLineFeed bool = false
-const fileMode uint32 = 0777
 
 // ResultCSV holds a file descriptor and abstract create/write/close function
 type ResultCSV struct {
@@ -20,6 +19,46 @@ type ResultCSV struct {
 	isStandardOutput bool
 	file             *os.File
 	csvWriter        *csv.Writer
+}
+
+// NewCSV creates ResultCSV instance. It creates a file at provided path if the file does not exists yet, otherwise truncates it.
+// Empty path makes the ResultCSV instance printing directly to standard output.
+func NewCSV(filePath string) (*ResultCSV, error) {
+
+	var file *os.File
+	var isStdout bool
+	var err error
+	if filePath == "" {
+		logrus.Info("No output file provided, dumping directly to stdout")
+		isStdout = true
+		file = os.Stdout
+	} else {
+		logrus.Infof("Opening output file %s", filePath)
+
+		file, err = os.Create(filePath)
+		if err != nil {
+			logrus.Error("error opening file: ", err)
+			return nil, err
+		}
+		err = os.Chmod(filePath, 0777)
+		if err != nil {
+			logrus.Error("error setting permissions on file: ", err)
+			return nil, err
+		}
+		isStdout = false
+	}
+
+	csvWriter := csv.NewWriter(file)
+	csvWriter.Comma = comma
+	csvWriter.UseCRLF = useCarriageReturnThenLineFeed
+	return &ResultCSV{
+		path:             filePath,
+		lock:             &sync.Mutex{},
+		file:             file,
+		csvWriter:        csvWriter,
+		isStandardOutput: isStdout,
+	}, nil
+
 }
 
 // Write writes a single CSV record. A record is a slice of strings.
@@ -68,37 +107,4 @@ func (csv *ResultCSV) Close() error {
 		return fmt.Errorf("while closing profiling csv file: %s", err.Error())
 	}
 	return nil
-}
-
-// NewCSV creates ResultCSV instance. It creates a file at provided path if the file does not exists yet, otherwise truncates it.
-// Empty path makes the ResultCSV instance printing directly to standard output.
-func NewCSV(filePath string) (*ResultCSV, error) {
-
-	var file *os.File
-	var isStdout bool
-	var err error
-	if filePath == "" {
-		logrus.Info("No output file provided, dumping directly to stdout")
-		isStdout = true
-		file = os.Stdout
-	} else {
-		logrus.Infof("Opening output file %s", filePath)
-		file, err = os.OpenFile(filePath, os.O_CREATE|os.O_TRUNC, os.FileMode(fileMode))
-		if err != nil {
-			return nil, fmt.Errorf("while opening profiling dump file: %s", err.Error())
-		}
-		isStdout = false
-	}
-
-	csvWriter := csv.NewWriter(file)
-	csvWriter.Comma = comma
-	csvWriter.UseCRLF = useCarriageReturnThenLineFeed
-	return &ResultCSV{
-		path:             filePath,
-		lock:             &sync.Mutex{},
-		file:             file,
-		csvWriter:        csvWriter,
-		isStandardOutput: isStdout,
-	}, nil
-
 }
