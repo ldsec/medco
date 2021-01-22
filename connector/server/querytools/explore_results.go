213897d0dcbbb5052feedd40f06611ad08ffd5a8
Nicolas Freundler <nicolas.freundler@protonmail.ch>
2021-01-22 17:24:11 +0100

    more verbose logs in direct SQL calls of query tools

--- connector/server/querytools/explore_results.go
+++ connector/server/querytools/explore_results.go
@@ -13,19 +13,19 @@ import (
 
 // InsertExploreResultInstance is called when the query is created. A new row is inserted in explore query results table with status 'running'.
 func InsertExploreResultInstance(userID, queryName, queryDefinition string) (int, error) {
-	logrus.Debugf("inserting user ID %s, query name %s, query definition %s", userID, queryName, queryDefinition)
-	logrus.Debugf("SQL: %s", insertExploreResultInstance)
+	description := fmt.Sprintf("InsertExploreResultInstance (user ID %s, query name %s, query definition %s), SQL: %s", userID, queryName, queryDefinition, insertExploreResultInstance)
+	logrus.Debugf("running: %s", description)
 	row := utilserver.DBConnection.QueryRow(insertExploreResultInstance, userID, queryName, queryDefinition)
 	res := new(string)
 	err := row.Scan(res)
 	if err != nil {
-		err = fmt.Errorf("while scanning SQL record: %s", err.Error())
+		err = fmt.Errorf("while scanning SQL record: %s, in DB operation :%s", err.Error(), description)
 		return 0, err
 	}
-	logrus.Debug("successfully inserted")
+	logrus.Debugf("DB operation successful result %s, operation: %s", *res, description)
 	queryID, err := strconv.Atoi(*res)
 	if err != nil {
-		err = fmt.Errorf("while parsing integer string \"%s\": %s", *res, err.Error())
+		err = fmt.Errorf("while parsing integer string \"%s\": %s, in DB operation :%s,", *res, err.Error(), description)
 		return 0, err
 	}
 	return queryID, nil
@@ -37,6 +37,7 @@ func InsertExploreResultInstance(userID, queryName, queryDefinition string) (int
 // I2b2 non encrypted patient set ID should be the result instance id returned by I2B2 after a successful query in project for non encrypted data.
 // Null pointer for i2b2 (non) encrypted patient set ID is used to update NULL value in the table.
 func UpdateExploreResultInstance(queryID int, clearResultSetSize int, clearResultSet []int, i2b2EncryptedPatientSetID, i2b2NonEncryptedPatientSetID *int) error {
+	var description string
 	var res sql.Result
 	var err error
 	setStrings := make([]string, len(clearResultSet))
@@ -48,36 +49,43 @@ func UpdateExploreResultInstance(queryID int, clearResultSetSize int, clearResul
 		err = fmt.Errorf("I2B2 patient set is undefined for both non encrypted and encrypted projects")
 		return err
 	}
-	logrus.Debugf("updating result instance queryID %d clear result set size %d clear result set %s  encrypted patientSetID %p non encrypted patient set ID %p", queryID, clearResultSetSize, setDefinition, i2b2EncryptedPatientSetID, i2b2NonEncryptedPatientSetID)
 	if i2b2EncryptedPatientSetID == nil {
-		logrus.Debugf("non encrypted patient set adress %p, value %d", i2b2NonEncryptedPatientSetID, *i2b2NonEncryptedPatientSetID)
-		logrus.Debugf("SQL: %s", updateExploreResultInstanceOnlyClear)
+		description = fmt.Sprintf("UpdateExploreResultInstanceOnlyClear (query ID: %d, clear patient set size %d, set definition: %s,clear patient set %d): SQL: %s", queryID, clearResultSetSize, setDefinition, *i2b2NonEncryptedPatientSetID, updateExploreResultInstanceOnlyClear)
+		logrus.Debugf("running: %s", description)
 		res, err = utilserver.DBConnection.Exec(updateExploreResultInstanceOnlyClear, queryID, clearResultSetSize, setDefinition, *i2b2NonEncryptedPatientSetID)
 	} else if i2b2NonEncryptedPatientSetID == nil {
-		logrus.Debugf("encrypted patient set adress %p, value %d", i2b2EncryptedPatientSetID, *i2b2EncryptedPatientSetID)
-		logrus.Debugf("SQL: %s", updateExploreResultInstanceOnlyEncrypted)
+		description = fmt.Sprintf("UpdateExploreResultInstanceOnlyEncrypted (query ID: %d, clear patient set size %d, set definition: %s, encrypted patient set: %d): SQL: %s", queryID, clearResultSetSize, setDefinition, *i2b2EncryptedPatientSetID, updateExploreResultInstanceOnlyEncrypted)
+		logrus.Debugf("running: %s", description)
 		res, err = utilserver.DBConnection.Exec(updateExploreResultInstanceOnlyEncrypted, queryID, clearResultSetSize, setDefinition, *i2b2EncryptedPatientSetID)
 	} else {
-		logrus.Debugf("non encrypted patient set adress %p, value %d", i2b2NonEncryptedPatientSetID, *i2b2NonEncryptedPatientSetID)
-		logrus.Debugf("encrypted patient set adress %p, value %d", i2b2EncryptedPatientSetID, *i2b2EncryptedPatientSetID)
-		logrus.Debugf("SQL: %s", updateExploreResultInstanceBoth)
+		description = fmt.Sprintf(
+			"UpdateUpdateExploreResultInstanceBoth (query ID: %d, clear patient set size %d, set definition: %s, clear patient set: %d, encrypted patient set: %d), SQL: %s",
+			queryID,
+			clearResultSetSize,
+			setDefinition,
+			*i2b2NonEncryptedPatientSetID,
+			*i2b2EncryptedPatientSetID,
+			updateExploreResultInstanceOnlyEncrypted,
+		)
+		logrus.Debugf("running: %s", description)
 		res, err = utilserver.DBConnection.Exec(updateExploreResultInstanceBoth, queryID, clearResultSetSize, setDefinition, *i2b2EncryptedPatientSetID, *i2b2NonEncryptedPatientSetID)
 	}
 	if err != nil {
-		err = fmt.Errorf("while executing SQL: %s", err.Error())
+		err = fmt.Errorf("while executing SQL: %s, DB operation: %s", err.Error(), description)
 		return err
 	}
-	logrus.Debug("successfully updated")
-	logrus.Tracef("sql execution result %+v", res)
+
 	affected, err := res.RowsAffected()
 	if err != nil {
-		err = fmt.Errorf("while checking SQL row affected: %s", err.Error())
+		err = fmt.Errorf("while checking SQL row affected: %s, SQL object result: %+v, DB operation: %s", err.Error(), res, description)
 		return err
 	}
 	if affected == 0 {
-		err = fmt.Errorf("nothing updated")
+		err = fmt.Errorf("no error with DB, but nothing was updated, DB operation: %s", description)
 		return err
 	}
+
+	logrus.Debugf("DB operation successful, number of affected rows %d, operation: %s", affected, description)
 
 	return nil
 
@@ -86,59 +94,67 @@ func UpdateExploreResultInstance(queryID int, clearResultSetSize int, clearResul
 // UpdateErrorExploreResultInstance updates the instance corresponding to the given queryID. Its status is changed to 'error'.
 // UpdateErrorExploreResultInstance should be called whenever any I2B2 project throws an error while executing a query.
 func UpdateErrorExploreResultInstance(queryID int) error {
-	logrus.Debugf("updating error for query instance: %d", queryID)
+	description := fmt.Sprintf("UpdateErrorExploreResultInstance (query ID: %d), SQL: %s", queryID, updateErrorExploreQueryInstance)
+	logrus.Debugf("running: %s", description)
 	res, err := utilserver.DBConnection.Exec(updateErrorExploreQueryInstance, queryID)
 	if err != nil {
-		err = fmt.Errorf("while executing SQL: %s", err.Error())
+		err = fmt.Errorf("during DB operation execution: %s, DB operation: %s", err.Error(), description)
 		return err
 	}
-	logrus.Debug("successfully updated")
-	logrus.Tracef("sql execution result %+v", res)
 	affected, err := res.RowsAffected()
 	if err != nil {
-		err = fmt.Errorf("while checking SQL row affected: %s", err.Error())
+		err = fmt.Errorf("while checking SQL row affected: %s, DB operation: %s", err.Error(), description)
 		return err
 	}
 	if affected == 0 {
-		err = fmt.Errorf("nothing updated")
+		err = fmt.Errorf("no error with DB, but nothing was updated, DB operation: %s", description)
 		return err
 	}
+	logrus.Debugf("DB operation successful, number of affected rows %d, operation: %s", affected, description)
+
 	return err
 }
 
 // CheckQueryID checks whether the user really has a query before inserting a new cohort defined by that query's id
 func CheckQueryID(userID string, queryID int) (bool, error) {
-	logrus.Debugf("selecting  user ID: %s , query ID: %d", userID, queryID)
-	logrus.Debugf("SQL: %s", checkQueryID)
+	description := fmt.Sprintf("CheckQueryID (user ID: %s, query ID: %d), SQL: %s", userID, queryID, checkQueryID)
+	logrus.Debugf("running: %s", description)
 	row := utilserver.DBConnection.QueryRow(checkQueryID, userID, queryID)
 	res := new(string)
 	err := row.Scan(res)
 	if err != nil {
-		err = fmt.Errorf("while scanning SQL record: %s", err.Error())
+		err = fmt.Errorf("while scanning SQL record: %s, DB operation: %s", err.Error(), description)
 		return false, err
 	}
-	logrus.Debug("successfully selected")
 	count, err := strconv.Atoi(*res)
 	if err != nil {
-		err = fmt.Errorf("while parsing integer string \"%s\": %s", *res, err.Error())
+		err = fmt.Errorf("while parsing integer string \"%s\": %s, Db operation: %s", *res, err.Error(), description)
 		return false, err
 	}
-	return (count > 0), err
+	retValue := count > 0
+
+	logrus.Debugf("DB operation successful, result: %t, DB operation: %s", retValue, description)
+
+	return retValue, err
 
 }
 
 // GetQueryDefinition is called when the query is created. A new row is inserted in explore query results table with status 'running'.
 func GetQueryDefinition(queryID int) (string, error) {
-	logrus.Debugf("query ID %d", queryID)
-	logrus.Debugf("SQL: %s", getQueryDefinition)
+
+	description := fmt.Sprintf("GetQueryDefinition (ID %d), SQL: %s", queryID, getQueryDefinition)
+	logrus.Debugf("running: %s", description)
+
 	row := utilserver.DBConnection.QueryRow(getQueryDefinition, queryID)
 	res := new(string)
 	err := row.Scan(res)
 	if err != nil {
-		err = fmt.Errorf("while scanning SQL record: %s", err.Error())
+		err = fmt.Errorf("while scanning SQL record: %s, DB operation: %s", err.Error(), description)
 		return "", err
 	}
-	logrus.Debug("successfully retrieved")
+
+	logrus.Debugf("DB operation successful, result: %s, DB operation: %s", *res, description)
+
 	return *res, nil
 
 }
