3ea6d17aa5cf60d947d8d8d258836d4ce7c3b12d
Mickaël Misbach <mickael@misba.ch>
2021-03-24 10:17:11 +0100

    rename variable

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-GECO_VERSION=$(git describe --tags 2> /dev/null)
+VERSION=$(git describe --tags 2> /dev/null)
 if [ $? -eq 0 ]; then
-  echo "$GECO_VERSION"
+  echo "$VERSION"
 else
   echo "v0.0.0-dev-$(git describe --tags --always)"
 fi
