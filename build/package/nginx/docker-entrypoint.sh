17410da45855839a16f801587a6525d02ecd0feb
Joao Sa Sousa <jagsousa1992@gmail.com>
2021-10-08 18:38:07 +0200

    Configure nginx.conf with stream using include directive + refactor

--- build/package/nginx/docker-entrypoint.sh
+++ build/package/nginx/docker-entrypoint.sh
@@ -4,19 +4,14 @@ set -Eeuo pipefail
 # apply configuration from environment variables
 pushd /etc/nginx/conf.d/
 
-# append stream directive to default configuration of nginx
-if [[ ${PROD_CONFIG} == "false" ]]; then
-  envsubst '${MEDCO_NODE_IDX} ${UNLYNX_PORT_0} ${UNLYNX_PORT_1}' < nginx.conf.template > ../nginx.conf
-else
-  envsubst '${UNLYNX_PORT_0} ${UNLYNX_PORT_1}' < nginx.conf.prod.template > ../nginx.conf
-fi
-
 envsubst '$HTTP_SCHEME' < servers.conf.template > servers.conf
 envsubst '$ALL_TIMEOUTS_SECONDS' < common/server-revproxy-base.conf.inc.template > common/server-revproxy-base.conf.inc
 
 if [[ ${PROD_CONFIG} == "false" ]]; then
+  envsubst '${MEDCO_NODE_IDX} ${UNLYNX_PORT_0} ${UNLYNX_PORT_1}' < nginx.conf.template > ../stream.conf
   cp common/server-revproxy-dev.conf.inc.template common/server-revproxy-dev.conf.inc
 else
+  envsubst '${UNLYNX_PORT_0} ${UNLYNX_PORT_1}' < nginx.conf.prod.template > ../stream.conf
   touch common/server-revproxy-dev.conf.inc
 fi
 
