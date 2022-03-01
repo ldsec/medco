a431f8db01734707b295add7c5844d807013bf2e
Mickaël Misbach <mickael@misba.ch>
2022-03-01 19:18:18 +0100

    nginx docker: remove http, allow only https

--- build/package/nginx/docker-entrypoint.sh
+++ build/package/nginx/docker-entrypoint.sh
@@ -4,7 +4,6 @@ set -Eeuo pipefail
 # apply configuration from environment variables
 pushd /etc/nginx/conf.d/
 
-envsubst '$HTTP_SCHEME' < servers.conf.template > servers.conf
 envsubst '$ALL_TIMEOUTS_SECONDS' < common/server-revproxy-base.conf.inc.template > common/server-revproxy-base.conf.inc
 
 if [[ ${PROD_CONFIG} == "false" ]]; then
